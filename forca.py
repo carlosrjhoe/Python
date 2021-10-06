18497756090141add5dcdd9dc9e7a559a671102c
Carlos Roberto <carlosrjhoe@gmail.com>
2021-10-06 20:53:51 -0300

    _061021_2055

--- forca.py
+++ forca.py
@@ -4,27 +4,39 @@ def jogar():
     print(texto.center(45))
     print('#' * 45)
 
-    palavra_secreta = 'banana'
-    letras_acertadas = ['_','_','_','_','_','_']
-    letras_faltando = str(letras_acertadas.count('_'))
-    print(f'Ainda faltam acertar {letras_faltando} letras')
+    palavra_secreta = 'banana'.upper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
-        chute = input('Qual a letra: ')
-        chute = chute.strip()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        chute = input('Qual a letra? ')
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        
+        enforcou = erros == 6
+        acertou = '_' not in letras_acertadas
         print(letras_acertadas)
-    print('Fim de jogo!')
+
+    if(acertou):
+        print('Você ganhou!!')
+    else:
+        print('Você perdeu!!')
+
+print('Fim de jogo!')
 
 if(__name__ == "__main__"): #garante a execução como programa principal:
     jogar()
