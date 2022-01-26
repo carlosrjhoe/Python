276401fc9fbe86a1d0205d20b4481d3c8d95cef4
Carlos Roberto <carlosrjhoe@gmail.com>
2022-01-26 19:55:33 -0300

    exe_01

---
+++ Curso_Eduardo_Mendes/ex_01.py
@@ -0,0 +1,11 @@
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite um numero: '))
+num3 = float(input('Digite um numero: '))
+
+resultado1 = (num1 * 2) * (num2 / 2)
+resultado2 = (num1 * 3) + num3
+resultado3 = num3 ** 3
+
+print(resultado1)
+print(resultado2)
+print(resultado3)
