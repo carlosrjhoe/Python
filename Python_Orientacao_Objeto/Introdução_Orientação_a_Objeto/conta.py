05bc4ef8737b637c7781ab95ea4f77a15423c273
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-05 13:28:20 -0300

    aprendendo o básico das classes

--- Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/conta.py
+++ Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/conta.py
@@ -1,5 +1,10 @@
-class Conta:
+class Conta:    # CLASS
 
-    def __init__(self):
-        print('Construindo objeto ... {self}')
-        self.numero
+    def __init__(self, numero, titular, saldo, limite): # FUNÇÃO CONSTUTORA
+        self.numero = numero    #ATRIBUTO
+        self.titular = titular  #ATRIBUTO
+        self.saldo = saldo  #ATRIBUTO
+        self.limite = limite    #ATRIBUTO
+        
+    def extrato(self):
+        print(f'O titular é {self.titular}, sua conta é {self.numero}, seu saldo: {self.saldo} e seu limite é de: {self.limite}')
