a2e26613d620306bc904214c3686fcdb21f8427a
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-05 15:07:55 -0300

    estudando classes, metodos, encapisulamento

--- Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/conta.py
+++ Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/conta.py
@@ -1,19 +1,24 @@
 class Conta:    # CLASS
 
     def __init__(self, numero, titular, saldo, limite): # FUNÇÃO CONSTUTORA
-        self.numero = numero    #ATRIBUTO
-        self.titular = titular  #ATRIBUTO
-        self.saldo = saldo  #ATRIBUTO
-        self.limite = limite    #ATRIBUTO
+        self.__numero = numero    #ATRIBUTO
+        self.__titular = titular  #ATRIBUTO
+        self.__saldo = saldo  #ATRIBUTO
+        self.__limite = limite    #ATRIBUTO
         
     def extrato(self):  # METODO MOSTRAR EXTRATO
-        print(f'O titular é {self.titular}, sua conta é {self.numero}, seu saldo: {self.saldo} e seu limite é de: {self.limite}')
+        print(f'O titular é {self.__titular}, sua conta é {self.__numero}, seu saldo: {self.__saldo} e seu limite é de: {self.__limite}')
 
     def deposita(self, valor):  # METODO DEPOSITAR VALOR
-        self.saldo += valor
+        self.__saldo += valor
     
     def sacar(self, valor):   # METODO SACAR VALOR
-        if valor > self.saldo:
+        if valor > self.__saldo:
             print('Saldo insuficiente')
         else:
-            self.saldo -= valor
+            self.__saldo -= valor
+    
+    def transferir(self, valor, destino):   # METODO TRANSFERIR
+        self.sacar(valor)
+        destino.deposita(valor)
+        
