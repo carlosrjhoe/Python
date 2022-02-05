52bc9cc5714b63dac3e438af7f7cfa28e4455639
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-05 14:36:59 -0300

    estudando class

--- Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/conta.py
+++ Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/conta.py
@@ -6,5 +6,14 @@ class Conta:    # CLASS
         self.saldo = saldo  #ATRIBUTO
         self.limite = limite    #ATRIBUTO
         
-    def extrato(self):
+    def extrato(self):  # METODO MOSTRAR EXTRATO
         print(f'O titular é {self.titular}, sua conta é {self.numero}, seu saldo: {self.saldo} e seu limite é de: {self.limite}')
+
+    def deposita(self, valor):  # METODO DEPOSITAR VALOR
+        self.saldo += valor
+    
+    def sacar(self, valor):   # METODO SACAR VALOR
+        if valor > self.saldo:
+            print('Saldo insuficiente')
+        else:
+            self.saldo -= valor
