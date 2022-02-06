a587ec22e5218f21c08fff5d191d8d6c77cb8d28
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-06 19:57:45 -0300

    Achei um pequeno erro no código, que demorei a achar

--- Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/Avançando_Orientação_a_Objeto/modelo.py
+++ Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/Avançando_Orientação_a_Objeto/modelo.py
@@ -1,11 +1,9 @@
-from os import link
-
-
 class Programa:
-    def __init__(self, nome, ano, likes):
+    
+    def __init__(self, nome, ano):
         self._nome = nome.title() # O title() é um método que retorna uma string onde o primeiro caractere em cada palavra é maiúsculo.
-        self._ano = ano
-        self._likes = likes
+        self.ano = ano
+        self._likes = 0
         
     @property
     def likes(self):
@@ -19,24 +17,28 @@ class Programa:
         return self._nome
     
     @nome.setter
-    def nome(self, novo_nome):
-        self._nome = novo_nome.title()
-        
-        
+    def nome(self, nome):
+        self._nome = nome
+
 class Filmes(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
         
 class Series(Programa):
-    def __init_(self, nome, ano, temporada):
+    def __init__(self, nome, ano, temporada):
         super().__init__(nome, ano)
         self.temporada = temporada
-    
-    
+
+
 vingadores = Filmes('vingadores', 2018, 150)
 smallville = Series('smallville', 2012, 10)
+vingadores.dar_like()
+vingadores.dar_like()
+smallville.dar_like()
+vingadores.dar_like()
+smallville.dar_like()
 
-print(f'Filme: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} Likes: {vingadores.likes}')
-print(f'Série: {smallville.nome} - Ano: {smallville.ano} - Temporada: {smallville.temporada} Likes: {smallville.likes}')
+print(f'Filme: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+print(f'Série: {smallville.nome} - Ano: {smallville.ano} - Temporada: {smallville.temporada} - Likes: {smallville.likes}')
 
