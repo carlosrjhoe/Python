97b745e0fd3fee9ab99a0ce5e56cbf8b8874bc84
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-06 14:27:10 -0300

    melhorando aprendizado quanto a get, set e @property

--- Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/Avançando_Orientação_a_Objeto/modelo.py
+++ Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/Avançando_Orientação_a_Objeto/modelo.py
@@ -1,20 +1,52 @@
 class Filmes:
-    
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title() # O title() é um método que retorna uma string onde o primeiro caractere em cada palavra é maiúsculo.
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
+        
+    @property
+    def likes(self):
+        return self.__likes
+        
+    def dar_like(self):
+        self.__likes += 1
+        
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome.title()
         
 class Series:
-    
     def __init__(self, nome, ano, temporada):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.temporada = temporada
+        self.__likes = 0
+    
+    @property
+    def likes(self):
+        return self.__likes
         
+    def dar_like(self):
+        self.__likes += 1
         
-vingadores = Filmes('Vingadores', 2018, 150)
-Smallville = Series('Smallville', 2012, 10)
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome.title()
+    
+        
+vingadores = Filmes('vingadores', 2018, 150)
+smallville = Series('smallville', 2012, 10)
 
-print(f'Filme: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}')
-print(f'Série: {Smallville.nome} - Ano: {Smallville.ano} - Temporada: {Smallville.temporada}')
+
+print(f'Filme: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} Likes: {vingadores.likes}')
+print(f'Série: {smallville.nome} - Ano: {smallville.ano} - Temporada: {smallville.temporada} Likes: {smallville.likes}')
+
