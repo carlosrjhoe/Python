7125b9a6fef93a3487babfd06c1bbb182923a3fc
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-06 19:30:42 -0300

    Aprendendo herança

--- Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/Avançando_Orientação_a_Objeto/modelo.py
+++ Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/Avançando_Orientação_a_Objeto/modelo.py
@@ -1,51 +1,41 @@
-class Filmes:
+from os import link
+
+
+class Programa:
+    def __init__(self, nome, ano, likes):
+        self._nome = nome.title() # O title() é um método que retorna uma string onde o primeiro caractere em cada palavra é maiúsculo.
+        self._ano = ano
+        self._likes = likes
+        
+    @property
+    def likes(self):
+        return self._likes
+        
+    def dar_like(self):
+        self._likes += 1
+        
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+        
+        
+class Filmes(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title() # O title() é um método que retorna uma string onde o primeiro caractere em cada palavra é maiúsculo.
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
         
-    @property
-    def likes(self):
-        return self.__likes
-        
-    def dar_like(self):
-        self.__likes += 1
-        
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome.title()
-        
-class Series:
-    def __init__(self, nome, ano, temporada):
-        self.__nome = nome.title()
-        self.ano = ano
+class Series(Programa):
+    def __init_(self, nome, ano, temporada):
+        super().__init__(nome, ano)
         self.temporada = temporada
-        self.__likes = 0
     
-    @property
-    def likes(self):
-        return self.__likes
-        
-    def dar_like(self):
-        self.__likes += 1
-        
-    @property
-    def nome(self):
-        return self.__nome
     
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome.title()
-    
-        
 vingadores = Filmes('vingadores', 2018, 150)
 smallville = Series('smallville', 2012, 10)
-
 
 print(f'Filme: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} Likes: {vingadores.likes}')
 print(f'Série: {smallville.nome} - Ano: {smallville.ano} - Temporada: {smallville.temporada} Likes: {smallville.likes}')
