7bcaa29694e6884dac8786feb19b4232710c0298
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-06 09:49:42 -0300

    Aprendendo sobre propriendades @prophet

--- Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/conta.py
+++ Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/conta.py
@@ -22,7 +22,7 @@ class Conta:    # CLASS
         self.sacar(valor)
         destino.deposita(valor)
         
-    def get_saldo(self):    # "GET" Recupera o valor atribuido à chave que voce envia como argumento para o método.
+    def get_saldo(self):    # "GET" Me devolve o valor atribuido à chave que voce envia como argumento para o método.
         return self.__saldo
 
     def get_titular(self):
@@ -34,4 +34,33 @@ class Conta:    # CLASS
     def set_limite(self, limite):   # "SET" Permitem as operações de adição, atualização e remoção de elementos.
         self.__limite = limite
         
+'''
+Agora que vimos as propriedades, crie-os no lugar dos getters da classe Conta. Por exemplo, no lugar do get_saldo, teremos:
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+Da mesma forma, crie uma propriedade para o setter do atributo limite:
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
         
+No Python Console, dentro do próprio PyCharm, teste o código, crie uma conta e acesse o valor de algum atributo, utilizando somente o seu nome, por exemplo:
+
+    >>> from conta import Conta
+    >>> conta = Conta(123, "Nico", 55.5, 1000.0)
+    Construindo objeto ... <conta.Conta object at 0x7f82af89d048>
+    >>> conta.limite
+    1000.0
+    
+Do mesmo jeito, altere o valor do atributo limite, deve funcionar como se você estivesse acessando-o diretamente:
+
+    >>> from conta import Conta
+    >>> conta = Conta(123, "Nico", 55.5, 1000.0)
+    Construindo objeto ... <conta.Conta object at 0x7f82af89d048>
+    >>> conta.limite = 2000.0
+    >>> conta.limite
+    2000.0
+'''
