d190cc2bdc4f1653a58d9039f1a0a55670e763db
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-06 22:01:35 -0300

    Aprendendo herança de um tipo built-in, herança iteravel e as devantagens de fazer herança

--- Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/Avançando_Orientação_a_Objeto/modelo.py
+++ Python_Orientacao_Objeto/Introdução_Orientação_a_Objeto/Avançando_Orientação_a_Objeto/modelo.py
@@ -39,21 +39,52 @@ class Series(Programa):
     def __str__(self):
         return (f'{self._nome} - {self.ano} - {self.temporada} temporadas - {self._likes} likes')
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+        
+    @property
+    def listagem(self):
+        return self._programas
+    
+    @property
+    def tamanho(self):
+        return len(self._programas)
+        
 
 vingadores = Filmes('vingadores', 2018, 150)
 smallville = Series('smallville', 2012, 10)
-vingadores.dar_like()
-vingadores.dar_like()
-smallville.dar_like()
-vingadores.dar_like()
-smallville.dar_like()
+tmep = Filmes('todo mundo em panico', 1999, 100)
+demolidor = Series('demolidor', 2016, 2)
 
-filmes_e_series = [vingadores, smallville]
-for programa in filmes_e_series:
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+vingadores.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+smallville.dar_like()
+vingadores.dar_like()
+smallville.dar_like()
+demolidor.dar_like()
+tmep.dar_like()
+
+filmes_e_series = [vingadores, smallville, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana.listagem)}')
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
     
     
 '''
+SOBRE __INIT__:
+    É um método, que por definição, é executada sempre que instanciamos um objeto.
+    
+SOBRE SELF:
+    O self é um parâmetro obrigatório que receberá a instância criada. Ao contrário de muitas linguagens, ele deve ser explícito. E também ao contrário de muitas linguagem que criam o objeto durante o construtor, Python cria o objeto e passa ele para o construtor complementar com as primeiras ações necessárias quando ele é construído.
+
 SOBRE @PROPERTY:
     Você pode definir propriedades com a sintaxe @property, que é mais compacta e legível.
     O uso de @property pode ser considerada a maneira "pytônica" de definir getters, setters e deleters.
