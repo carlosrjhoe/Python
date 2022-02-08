910a758e674a58b49d3e0d5f8a38fd70ea778930
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-08 12:58:54 -0300

    resolvendo os exercicios

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Ex25.py
@@ -0,0 +1,6 @@
+num1 = int(input('Digite um número: '))
+
+if (num1 % 2 == 0):
+    print(f'O número é PAR.')
+else:
+    print(f'O número é IMPAR.')
---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Ex27.py
@@ -0,0 +1,2 @@
+for i in 'Nokia tisla':
+    print(i)
