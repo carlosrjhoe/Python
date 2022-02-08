c8e318de450f961119144680042fc28d643ee40d
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-08 13:29:08 -0300

    Rexolvendo exercicios até o 30

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Ex28.py
@@ -0,0 +1,4 @@
+lista_Super_Mercado = ['Água', 'Açucar', 'Arroz', 'Pão', 'leite', 'Massa', 'Carne', 'Refrigerante']
+
+for i in lista_Super_Mercado:
+    print(i)
---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Ex29.py
@@ -0,0 +1,5 @@
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+for i in range(num1, num2+1):
+    print(i)
