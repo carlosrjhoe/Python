2911843ed13350026972114c8032b4d02d2d525f
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-10 13:17:59 -0300

    exercicio 35

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Ex_35_numeros_primos.py
@@ -0,0 +1,14 @@
+num = int(input('Digite um número: '))
+divisoes = 0
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        divisoes += 1
+        '''Criei um laço para percorrer de "i" em range, e se "i" o resto da divisão for igual a 0, implementa a variavel "divisao" + 1'''
+if divisoes == 2:
+    '''Se a variavel "divisoes" for igual a 2, a variavel "num" é primo'''
+    print(f'{num} é primo')
+    print(f'{num} é divisivel por 1 ou por {num}')
+else:
+    print(f'{num} não é primo')
+    print(f'{} é divisível {} vezes...')
