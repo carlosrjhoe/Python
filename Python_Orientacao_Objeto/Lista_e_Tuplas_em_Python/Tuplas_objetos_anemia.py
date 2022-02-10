6ef01a1946656c075c7211483409cba969c6ec49
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-09 21:03:38 -0300

    Tuplas, objeto e anemia

---
+++ Python_Orientacao_Objeto/Lista_e_Tuplas_em_Python/Tuplas_objetos_anemia.py
@@ -0,0 +1,7 @@
+carlos = ['carlos', 36, 1985]
+mayara = ['mayara', 37, 1984]
+
+usuarios = (carlos, mayara)
+usuarios.append(('neto', 6, 2015))
+'''Uma tupla é imutável. Ou seja, não se pode adicionar um elemento.'''
+print(usuarios)
