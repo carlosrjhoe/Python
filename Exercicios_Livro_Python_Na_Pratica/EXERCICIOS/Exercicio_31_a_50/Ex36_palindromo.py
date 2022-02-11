87a245f20b0491f59cf2cb9ad0d21e737c27e351
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-11 12:54:00 -0300

    falta completar exercicio palindromo

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicio_31_a_50/Ex36_palindromo.py
@@ -0,0 +1,13 @@
+frase = str(input('Digite uma palavra ou frase: ')).strip().upper()
+'''
+upper():
+    Retorna uma cópia da string em MAIÚSCULAS.
+strip():
+    Retorna uma cópia da string com os caracteres iniciais e finais removidos.
+'''
+
+palavras = frase.split()
+caractere = ".join(palavras)fraseinvertida="
+for i in range(len(caractere)-1,-1,-1):
+    fraseinvertida += caractere[i]
+print(caractere, fraseinvertida)
