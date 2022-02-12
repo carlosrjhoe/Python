008f5c74684216300747eb06f2b987775b922430
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-11 21:33:44 -0300

    atualiza

--- Python_Orientacao_Objeto/Validação_de_Dados/Expressoes_regolares/main.py
+++ Python_Orientacao_Objeto/Validação_de_Dados/Expressoes_regolares/main.py
@@ -1,5 +1,9 @@
-from telefones import Telefones
+from telefones import TelefonesBr
 import re
 
-telefone = '8195419951'
-telefone_objeto = Telefones(telefone)
+padrao_molde = "(xx)aaaa-wwww"
+padrao = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
+texto = "eu gosto do numero 2126451234"
+resposta = re.search(padrao,texto)
+
+print(resposta.group())
--- Python_Orientacao_Objeto/Validação_de_Dados/Expressoes_regolares/telefones.py
+++ Python_Orientacao_Objeto/Validação_de_Dados/Expressoes_regolares/telefones.py
@@ -1,17 +1,16 @@
 import re
 
-class Telefones:
-    
+class TelefonesBr:
     def __init__(self, telefone):
         if self.valida_telefone(telefone):
             self.numero = telefone
         else:
-            raise ValueError("Numero incorreto.")
-    
+            raise ValueError("Número incorreto!")
+
     def valida_telefone(self, telefone):
         padrao = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
-        respota = re.findall(padrao, telefone)
-        if respota:
+        resposta = re.findall(padrao,telefone)
+        if resposta:
             return True
         else:
             return False
