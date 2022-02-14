4d9a35ec6d3cd7357e50fd9ff7bd5fa67272c130
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-14 13:06:51 -0300

    Exercicio concluido

--- Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicio_31_a_50/Ex36_palindromo.py
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicio_31_a_50/Ex36_palindromo.py
@@ -1,13 +1,19 @@
 frase = str(input('Digite uma palavra ou frase: ')).strip().upper()
+
+palavras = frase.split()
+caractere = ''.join(palavras)
+frase_invertida = ''
+for i in range(len(caractere)-1,-1,-1):
+    frase_invertida += caractere[i]
+
+if frase_invertida == caractere:
+    print('É um palindromo.')
+else:
+    print('Não é um palindromo.')
+
 '''
 upper():
     Retorna uma cópia da string em MAIÚSCULAS.
 strip():
     Retorna uma cópia da string com os caracteres iniciais e finais removidos.
 '''
-
-palavras = frase.split()
-caractere = ".join(palavras)fraseinvertida="
-for i in range(len(caractere)-1,-1,-1):
-    fraseinvertida += caractere
-print(caractere)
