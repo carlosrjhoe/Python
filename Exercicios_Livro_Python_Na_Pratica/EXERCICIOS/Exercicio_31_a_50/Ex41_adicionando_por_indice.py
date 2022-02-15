dc227516a2fef3627b93319aefa38493b7ee5519
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-15 12:31:08 -0300

    Exercio 41

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicio_31_a_50/Ex41_adicionando_por_indice.py
@@ -0,0 +1,10 @@
+from time import process_time_ns
+
+
+nomes = ['Ana', 'Carlos', 'Daniele', 'Fernando', 'Maria']
+nomes.insert(2, 'Mayara')
+print(nomes)
+nomes.insert(3, 'Neto')
+print(nomes)
+nomes.insert(4, 'Luna')
+print(nomes)
