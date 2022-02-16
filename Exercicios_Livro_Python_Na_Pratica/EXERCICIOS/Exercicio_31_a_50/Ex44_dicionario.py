8aea45156a922081d61956e47dc23bd517e6f43e
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-16 13:13:44 -0300

    exercicio dicionario

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicio_31_a_50/Ex44_dicionario.py
@@ -0,0 +1,10 @@
+dicionario = dict(
+    Pão = 'R$ 1,99',
+    Açucar = 'R$ 4,99',
+    Café = 'R$ 3,49',
+    Macarrão = 'R$ 3,99',
+    Carne = 'R$ 16,99',
+)
+
+print(type(dicionario))
+print(dicionario)
