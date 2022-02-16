b283e0a95e2ee50d721768b97574e83e14c8f1da
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-16 13:08:01 -0300

    exercicio 43

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicio_31_a_50/Ex43_fatiamnetos_de_elementos.py
@@ -0,0 +1,4 @@
+nomes = ['Ana', 'Carlos', 'Daniele', 'Fernando', 'Maria', 'Neto', 'Luna']
+
+print(nomes[1:4])   # IMPRIME OS NOMES DA 1 A 4 POSIÇÃO DA LISATA "nomes"
+print(nomes[-1])    # IMPRIME APENAS O ULTIMO NOME DA LISA.
