c4b20f61d94b1b57c045466441f2c737cee975f4
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-17 12:41:01 -0300

    exercicios 44, 45 e 46

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicio_31_a_50/Ex45_criando_dicionario_de_lista.py
@@ -0,0 +1,6 @@
+lista = ['caneta', 'Lápis', ' Borracha', 'caderno']
+precos = ['1,99', '0,99', '0,50', '9,90']
+
+dicionario = dict(keys = lista, values = precos)
+print(type(dicionario))
+print(dicionario)
---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicio_31_a_50/Ex46_cadastro.py
@@ -0,0 +1,12 @@
+cadastro_pessoa = {
+    'Sexo': 'Masculino',
+    'Idade': 36,
+    'Nacionalidade': 'Brasil',
+    'Estado Civil': 'Casado',
+    'Escolaridade': ['Superior', 'Doutorado'],
+    'Ocupação': 'Estudante',
+    'Renda': '2000.00 - 299999'
+}
+
+print(type(cadastro_pessoa))
+print(cadastro_pessoa)
