d208857d2a0c1a66b05039d5897518eddc7ec37b
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-21 19:22:53 -0300

    deletando arquivos para organizar pasta

--- Curso_Eduardo_Mendes/Curso_Selenium/Aula_02/aula_03.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
-
-url = 'https://curso-python-selenium.netlify.app/aula_03.html'
-driver.get(url)
-
-a = driver.find_element_by_tag_name('a')
-p = driver.find_element_by_tag_name('p')
--- Curso_Eduardo_Mendes/Curso_Selenium/Aula_02/teste.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
-driver.get('www.xvideos.com.br')
-
-driver.quit() # FECHA A PAGINA
