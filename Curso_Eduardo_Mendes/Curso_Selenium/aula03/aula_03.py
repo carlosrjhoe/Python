b318e7689f118d3f22f31c6fd6bd830d36c6e2f9
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-22 12:21:45 -0300

    Atualizar arquivo

--- Curso_Eduardo_Mendes/Curso_Selenium/aula03/aula_03.py
+++ Curso_Eduardo_Mendes/Curso_Selenium/aula03/aula_03.py
@@ -1,5 +1,5 @@
-from selenium.webdriver import Chrome
-from time import sleep
+from selenium.webdriver import Chrome # importa Selenium.webdriver
+from time import sleep # Importa o modulo sleep da biblioteca 
 
 url = 'https:/curso-python-selenium.netlify.app/aula_03.html'
 driver = Chrome()
@@ -7,7 +7,6 @@ driver.get(url)
 sleep(1)
 
 a = driver.find_element_by_tag_name('a')
-
 
 for click in range(11):
     ps = driver.find_elements_by_tag_name('p')
