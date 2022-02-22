d09ae814517090a52fd21c93c4860bcb6a5e9d0c
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-21 21:06:27 -0300

    Minha primeira automação: abrir uma pagina e buscar a tag: a

--- Curso_Eduardo_Mendes/Curso_Selenium/aula03/aula_03.py
+++ Curso_Eduardo_Mendes/Curso_Selenium/aula03/aula_03.py
@@ -1,4 +1,17 @@
 from selenium.webdriver import Chrome
+from time import sleep
 
+url = 'https:/curso-python-selenium.netlify.app/aula_03.html'
 driver = Chrome()
-driver.get('https:/www.google.com.br')
+driver.get(url)
+sleep(1)
+
+a = driver.find_element_by_tag_name('a')
+
+
+for click in range(11):
+    ps = driver.find_elements_by_tag_name('p')
+    a.click()
+    print(f'Valor de p: {ps[-1].text} Valor do click: {click}')
+    print(f'Os valores são iguais {ps[-1].text == str(click)}')
+driver.quit()
