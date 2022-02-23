ed30afe29ab5036408075e4e2c0356878514fba7
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-23 19:33:40 -0300

    Treinando dicionario com python

---
+++ Python_Orientacao_Objeto/Exercicio_Python/dicionarios.py
@@ -0,0 +1,12 @@
+pessoa = {
+    'nome1': 'Carlos',
+    'idade1': 36,
+    'nome2': 'May',
+    'idade2': 37,
+    'nome3': 'Neto',
+    'idade3': 6,
+    'nome4': 'Luna',
+    'idade4': 4,
+}
+for nome in pessoa.items():
+    print(f'{nome}')
