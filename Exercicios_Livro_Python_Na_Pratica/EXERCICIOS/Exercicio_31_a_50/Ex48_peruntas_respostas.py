be81e9d45f86a23efc21d5e89b813c2766fb2daa
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-25 13:02:31 -0300

    Resolvendo exercicio 48

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicio_31_a_50/Ex48_peruntas_respostas.py
@@ -0,0 +1,25 @@
+base = {
+    'pergunta01': {
+        'pergunta':'Quanto é 4 x 4?',
+        'alternativa':{'a':'12','b':'24','c':'16','d':'20'},
+        'respostas_certa':'c'
+    },
+    'pergunta02': {
+        'pergunta':'Quanto é 6 / 3?',
+        'alternativa':{'a':'2.5','b':'3','c':'2','d':'1.5'},
+        'respostas_certa':'c'
+    }
+}
+
+respostas_certas = 0
+
+for pkeys, pvalues in base.items():
+    print(f'{pkeys}:{pvalues["pergunta"]}')
+    
+    for rkeys, rvalues in pvalues['alternativas'].item():
+        print(f[{'rkeys'}]:{rvalues})
+        
+    resposta = input('Escolha uma alternativa: [a][b][c] ou [d]')
+    
+    if resposta == pvalues['Cesposta Correta']:
+        
