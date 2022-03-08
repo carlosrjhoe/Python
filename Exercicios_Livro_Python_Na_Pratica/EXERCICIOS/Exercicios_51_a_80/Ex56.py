8cf4c259231fcd17281a51d398df72d31ef3559f
Carlos Roberto <carlosrjhoe@gmail.com>
2022-03-08 12:27:03 -0300

    Resolução exercicio 56

--- Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_51_a_80/Ex56.py
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_51_a_80/Ex56.py
@@ -2,5 +2,6 @@ def boas_vindas(nome, nacionalidade='Brasileiro'):
     return f'Bem vindo(a) {nome} {nacionalidade}'
 
 nome = input('Digite o nome: ')
+nacionalidade = input('Digite sua nacionalidade: ')
 
-print(boas_vindas(nome))
+print(boas_vindas(nome)
