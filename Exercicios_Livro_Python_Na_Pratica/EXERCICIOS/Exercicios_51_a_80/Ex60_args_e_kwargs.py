4265103188efbb799ed8f0587c3564f3c1e15c77
Carlos Roberto <carlosrjhoe@gmail.com>
2022-03-09 13:19:09 -0300

    Resolução do exercicio 60

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_51_a_80/Ex60_args_e_kwargs.py
@@ -0,0 +1,13 @@
+'''Crie uma função que recebe parâmetros tanto por justaposição (*args) quanto nomeados (**kwargs)'''
+
+def identificacao(*args, **kwargs):
+    for n in args:
+        nome = n
+        print(f'{n}')
+        
+        for k, v in kwargs.items():
+            idade = k
+            sexo = v
+            print(f'Nome: {nome}, {idade}, {sexo}')
+            
+identificacao('fernando', idade = 33, sexo = 'M')
