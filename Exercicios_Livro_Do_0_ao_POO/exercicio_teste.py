7f69b03c62da137444610d5f5aaeae1ba34fb1fc
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-01 05:10:37 -0300

    relebrando comandos

---
+++ Exercicios_Livro_Do_0_ao_POO/exercicio_teste.py
@@ -0,0 +1,11 @@
+''' Crie um pequeno programa que mostre os números de 1 a 10 usando um
+loop for. Em seguida, crie um programa equivalente que mostre os números
+de 1 a 10 usando um loop while. '''
+
+for item in range(0, 11):
+  print(item)
+
+numero = 0
+while numero <= 10:
+  print(numero)
+  numero += 1
