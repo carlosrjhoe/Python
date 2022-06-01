74132a951813aa2cbb3d62549aae80b9f23c4748
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-01 11:24:28 -0300

    relebrando comandos

---
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/Expressões lógicas/ex_01.py
@@ -0,0 +1,12 @@
+''' Nesses casos, os operadores relacionais devem ser avaliados primeiramente. Fa-
+çamos salário = 100 e idade = 20. Teremos: '''
+
+# Exercício 3.4
+
+salario = 5000
+
+if salario > 1200:
+  print("Deve pagar imposto")
+else:
+  print("Não precisa pagar imposto.")
+
