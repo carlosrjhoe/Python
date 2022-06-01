7f1c25a55d043e698a93266c260cd201ee6b4cb6
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-01 10:45:22 -0300

    relebrando comandos

---
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/Not/ex_01.py
@@ -0,0 +1,12 @@
+"""Complete a tabela a seguir utilizando a = True, b = False e c = True"""
+
+a = True
+b = False
+c = True
+
+print(a and b)
+print(a and c)
+print(b and c)
+print(not a)
+print(not b)
+print(not c)
---
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/Operadores_relacionais/ex_01.py
@@ -0,0 +1,14 @@
+a = 1
+b = 5
+
+print(a == b)
+print(a > b)
+print(a < b)
+print(a > b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+reprovado = a > b
+
+print(reprovado)
+
