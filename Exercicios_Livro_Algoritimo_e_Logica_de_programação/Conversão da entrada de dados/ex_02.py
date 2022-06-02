9afc5d54e7bb58781a770503bc5967dc0382896f
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-01 21:28:38 -0300

    relebrando comandos

---
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/Conversão da entrada de dados/ex_02.py
@@ -0,0 +1,8 @@
+''' Exercício 3.7 Faça um programa que peça dois números inteiros. Imprima a soma
+desses dois números na tela. '''
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+soma = num1 + num2
+
+print(f'A soma de {num1} + {num2} é = {soma}')
---
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/Conversão da entrada de dados/ex_03.py
@@ -0,0 +1,6 @@
+''' Exercício 3.8 Escreva um programa que leia um valor em metros e o exiba convertido em milímetros '''
+
+num = int(input('Digite um valor em [m]: '))
+milimetro = num * 1000
+
+print(f'O valor de {num}m, é equivalente a {milimetro}mm')
