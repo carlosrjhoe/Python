0cbf7032e2b10d4f1d94f0f768d696ca2dcbe554
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-03 05:04:18 -0300

    relembrando alguns comandos

--- Curso_Eduardo_Mendes/exercicios/ex_04.py
+++ Curso_Eduardo_Mendes/exercicios/ex_04.py
@@ -1,2 +1,6 @@
-nome = 'Carlos'
-print(nome)
+# nome = 'Carlos'
+# print(nome)
+
+resposta = input('Qual sua data de nsacimento? ')
+dia, mes, ano = resposta.split('/')
+print(f'Você nasceu em {dia} do {mes} de {ano}')
