4c777094118258983cb92fb73448e0c7c5fe7fd0
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-03 20:51:09 -0300

    Atualizando arquivos

--- Curso_Eduardo_Mendes/exercicios/ex_01.py
+++ Curso_Eduardo_Mendes/exercicios/ex_01.py
@@ -6,6 +6,6 @@ resultado1 = (num1 * 2) * (num2 / 2)
 resultado2 = (num1 * 3) + num3
 resultado3 = num3 ** 3
 
-print(resultado1)
-print(resultado2)
-print(resultado3)
+print(f'{resultado1:.2f}')
+print(f'{resultado2:.2f}')
+print(f'{resultado3:.2f}')
