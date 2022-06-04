af8acb487ab292a295b697e687531ca8ce0f4e5f
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-03 23:03:20 -0300

    Exercicio 4

--- Exercicios_Livro_Algoritimo_e_Logica_de_programação/IF/ex_04.py
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/IF/ex_04.py
@@ -1,3 +1,11 @@
 """Exercício 4.3 Escreva um programa que leia três números e que imprima o maior
 e o menor."""
 
+lista = []
+
+for i in range(3):
+   numero = int(input('Digite um numero: '))
+   lista.append(numero)
+
+print(f'O maior é: {max(lista)}')
+print(f'O menor é: {min(lista)}')
