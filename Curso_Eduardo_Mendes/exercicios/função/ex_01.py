48dcaa8f5528d81d2491ee0721f996b1537fced9
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-03 22:44:22 -0300

    Atualizando arquivos de estudos

---
+++ Curso_Eduardo_Mendes/exercicios/função/ex_01.py
@@ -0,0 +1,8 @@
+def mostra_nome(nome):
+   print(nome)
+
+def mostra_data(data):
+   print(data)
+
+mostra_nome('Carlos')
+mostra_data('3/11/1985')
---
+++ Curso_Eduardo_Mendes/exercicios/função/ex_02.py
@@ -0,0 +1,10 @@
+numeros = [1, 2, 3, 4, 5]
+
+def soma(a, b):
+   return a + b
+
+def media(lista_de_numeros):
+   return sum(numeros) / len(numeros)
+
+print(soma(1, 2))
+print(media(numeros))
