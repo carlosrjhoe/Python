9a8e2ec38b81c3545edd233bcfb3546ac5b88cbd
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-06 05:21:58 -0300

    Melhorando a plicação de estudo de Selenium

--- Curso_Selenium_Eduardo_Mendes/Selenium/venv/aula_03.py
+++ Curso_Selenium_Eduardo_Mendes/Selenium/venv/aula_03.py
@@ -1,3 +1,6 @@
+from cgitb import text
+from contextlib import closing
+from typing import Text
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -5,11 +8,20 @@ from time import sleep
 url = 'https://curso-python-selenium.netlify.app/aula_03.html'
 driver = Chrome()
 driver.get(url)
-sleep(2)
+sleep(3)
 
 a = driver.find_element(By.TAG_NAME, 'a')
-p = driver.find_element(By.TAG_NAME, 'p')
+
 print(f'Texto de a: {a.text}')
-print(f'texto de p: {p.text}')
+
+for click in range(11):
+    # Buscar um elemento
+    ps = driver.find_elements(By.TAG_NAME, 'p')
+    # Clicar em um elemento
+    a.click()
+    # Pegar o valor do elemento
+    print(f'texto de p: {int(ps[-1].text)}, valor do click: {click}')
+    # Verifirar variaveis
+    print({ps[-1].text} == {Text(click)})
 
 driver.close()
