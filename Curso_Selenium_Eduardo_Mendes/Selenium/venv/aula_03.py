8f478d09023930dc69d85f7215bc594c89aa6ea2
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-05 21:24:03 -0300

    Revendo alguns comando do selenium

--- Curso_Selenium_Eduardo_Mendes/Selenium/venv/aula_03.py
+++ Curso_Selenium_Eduardo_Mendes/Selenium/venv/aula_03.py
@@ -3,9 +3,13 @@ from selenium.webdriver.common.by import By
 from time import sleep
 
 url = 'https://curso-python-selenium.netlify.app/aula_03.html'
-browser = Chrome()
-browser.get(url)
+driver = Chrome()
+driver.get(url)
+sleep(2)
 
-a = browser.find_element(by=By.TAG_NAME, value='a')
-p = browser.find_element(by=By.TAG_NAME, value='p')
+a = driver.find_element(By.TAG_NAME, 'a')
+p = driver.find_element(By.TAG_NAME, 'p')
+print(f'Texto de a: {a.text}')
+print(f'texto de p: {p.text}')
 
+driver.close()
