fbd238a04188a7c9216a2bb4dddc5c53049bae89
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-07 12:48:38 -0300

    exercicio 04 resolvido

--- Exercicios_Livro_Algoritimo_e_Logica_de_programação/Contadores/ex_04.py
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/Contadores/ex_04.py
@@ -2,3 +2,12 @@
 multiplicação do primeiro pelo segundo. Utilize apenas os operadores de soma e
 subtração para calcular o resultado. Lembre-se de que podemos entender a multiplicação de dois números como somas sucessivas de um deles. Assim, 4 x 5 = 5
 + 5 + 5 + 5 = 4 + 4 + 4 + 4 + 4"""
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+x = 1
+r = 0
+while x <= num2:
+    r = r + num1
+    x = x + 1
+print(f'{num1} x {num2} = {r}')
