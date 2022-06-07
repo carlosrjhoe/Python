85829964fc866638a172d14b0d6c2be73d4925f6
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-06 21:24:29 -0300

    Resolvendo exercicios

---
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/Repetições/ex_01.py
@@ -0,0 +1,21 @@
+""" xercício 5.1 Modifique o programa para exibir os números de 1 a 100 """
+
+# x = 1
+# while x <= 100:
+#     print(x)
+#     x += 1
+
+""" Exercício 5.2 Modifique o programa para exibir os números de 50 a 100. """
+
+# x = 50
+# while x <= 100:
+#     print(x)
+#     x += 1
+
+""" Exercício 5.3 Faça um programa para escrever a contagem regressiva do lançamento
+# de um foguete. O programa deve imprimir 10, 9, 8, ..., 1, 0 e Fogo! na tela """
+
+x = 10
+while x >= 0:
+    print(x)
+    x -= 1
---
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/Repetições/exemplo1.py
@@ -0,0 +1,11 @@
+# x = 1
+# print(x)
+# x = 2
+# print(x)
+# x = 3
+# print(x)
+
+x = 1
+while x <= 3:
+    print(x)
+    x += 1
