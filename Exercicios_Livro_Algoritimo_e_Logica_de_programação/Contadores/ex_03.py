d2e7da764601815aac3c88b30c4e3acb7416ec6c
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-07 11:34:14 -0300

    Falta realizar a resolução dos exercicios 03, 04 e 05

---
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/Contadores/ex_03.py
@@ -0,0 +1,7 @@
+""""Exercício 5.7 Modifique o programa anterior de forma que o usuário também
+digite o início e o fim da tabuada, em vez de começar com 1 e 10""""
+
+n = int(input('Tubuada de: '))
+inicio = int(input('Qual inicio da tabuada: '))
+fim = int(input('Qual final da tabuada:'))
+
