a23c5242f8e09cb4a265eeb85d7b584b75e0f57f
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-08 05:30:29 -0300

    Resolvendo exercicios

---
+++ Livro_Curso_Intensivo_de_Python/String/exemplo.py
@@ -0,0 +1,20 @@
+name = "ada lovelace"
+print(name.title())
+# title() exibe cada palavra com uma letra maiúscula no início.
+
+print(name.upper())
+# upper() exibe todas palavras com uma letra maiúscula.
+
+print(name.lower())
+# lower() exibe todas palavras com letras minusculas.
+
+print('Linguagens:\n\tPython\n\tC++\n\tJavaScript\n\tJava')
+# A string "\n\t" diz a Python para passar para uma nova linha e
+# iniciar a próxima com uma tabulação.
+
+nome_inteiro = 'Carlos '
+nome_inteiro = nome_inteiro.rstrip()
+print(len(nome_inteiro))
+# O método rstrip() atua na variável nome_inteiro, esse espaço
+# extra é removido
+
