4ade976f18ef103613a283cfebc3ec4ee607beb5
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-08 05:29:51 -0300

    Resolvendo exercicios

---
+++ Livro_Curso_Intensivo_de_Python/Numeros/exemplo.py
@@ -0,0 +1,11 @@
+idade = 37
+# msg = 'Feliz ' + idade + 'rd aniversário'
+# É um erro de tipo. Significa que Python não é capaz de reconhecer o tipo
+# de informação que você está usando.
+
+msg = 'Feliz ' + str(idade) + 'rd aniversário'
+print(msg)
+
+print(3 % 2)
+
+import this
