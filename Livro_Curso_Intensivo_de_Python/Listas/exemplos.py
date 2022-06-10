4a5c35acb528a9a13851b69d9f1eae4a87c40a6d
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-09 22:00:40 -0300

    Estudando python

--- Livro_Curso_Intensivo_de_Python/Listas/exemplos.py
+++ Livro_Curso_Intensivo_de_Python/Listas/exemplos.py
@@ -1,6 +1,22 @@
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+"""bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
 
 print(bicycles[0].title())
 print(bicycles[2].title())
-print(bicycles[-1])
+print(bicycles[-1])"""
+
+nomes = ['carlos', 'mayara', 'neto', 'luna']
+print(nomes)
+nomes.append('rosiclé')
+print(nomes)
+nomes.insert(0, 'Lúcinha')
+print(nomes)
+del nomes[5]
+print(nomes)
+
+nomes_retirados = nomes.pop()
+print(nomes)
+print(nomes_retirados)
+nomes_retirados = nomes.pop()
+print(nomes)
+print(nomes_retirados)
