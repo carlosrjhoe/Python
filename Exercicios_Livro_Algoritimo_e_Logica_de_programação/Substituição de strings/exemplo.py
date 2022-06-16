a18610cbe729641bd6e956301981da9288c632bc
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-16 19:35:41 -0300

    estudando

---
+++ Exercicios_Livro_Algoritimo_e_Logica_de_programação/Substituição de strings/exemplo.py
@@ -0,0 +1,9 @@
+nomes = '////////////////um tigre'
+# print(nomes)
+# print(nomes.replace('tigre', 'gato'))
+# print(nomes.replace('tigre', 'gato', 3))
+
+"""Listagem 7.19 – Remoção de espaços em branco com strip, lstrip e rstrip"""
+print(nomes)
+print(nomes.strip(" "))
+print(nomes.lstrip('/'))
