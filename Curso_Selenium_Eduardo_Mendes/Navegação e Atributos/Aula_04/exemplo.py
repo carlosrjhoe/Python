706ae16b1b975dde4ef2363a35f1139ad97500b0
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-16 10:19:30 -0300

    Arquivos que não estavam sendo supervisionado

--- Curso_Selenium_Eduardo_Mendes/Navegação e Atributos/Aula_04/exemplo.py
+++ Curso_Selenium_Eduardo_Mendes/Navegação e Atributos/Aula_04/exemplo.py
@@ -2,9 +2,9 @@ from selenium.webdriver import Chrome
 
 driver = Chrome()
 driver.get('https://selenium.dunossauro.live/aula_04_a.html')
-lista_nao_ordenada = driver.find_elements_by_tag_name('ul') # 1
-lis = lista_nao_ordenada.find_elements_by_tag_name('li') # 2
-lis[0].find_elements_by_tag_name('a') # 3
+lista_nao_ordenada = driver.find_element_by_tag_name('ul') # 1
+lis = lista_nao_ordenada.find_element_by_tag_name('li') # 2
+lis[0].find_element_by_tag_name('a') # 3
 
 """
 1. Buscamos 'ul'
