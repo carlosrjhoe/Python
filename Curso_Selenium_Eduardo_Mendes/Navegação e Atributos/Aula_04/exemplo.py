13e81f47ca457d6652381271cbd2c8245a0e3ea8
Carlos Roberto <carlosrjhoe@gmail.com>
2022-07-17 21:14:09 -0300

    Atualizando arquivos

--- Curso_Selenium_Eduardo_Mendes/Navegação e Atributos/Aula_04/exemplo.py
+++ Curso_Selenium_Eduardo_Mendes/Navegação e Atributos/Aula_04/exemplo.py
@@ -1,10 +1,11 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 
 driver = Chrome()
 driver.get('https://selenium.dunossauro.live/aula_04_a.html')
-lista_nao_ordenada = driver.find_element_by_tag_name('ul') # 1
-lis = lista_nao_ordenada.find_element_by_tag_name('li') # 2
-lis[0].find_element_by_tag_name('a') # 3
+lista_nao_ordenada = driver.find_elements(By.TAG_NAME, "ul") # 1
+lis = lista_nao_ordenada.find_elements(By.TAG_NAME, "li") # 2
+# lis[0].find_element_by_tag_name('a') # 3
 
 """
 1. Buscamos 'ul'
---
+++ Livro_Automatize_tarefas_maçantes_com_Python/Basico_do_Python/teste.py
@@ -0,0 +1,10 @@
+# meu_nome = 'Carlos'
+# print(meuNome)
+
+# print(meu_nome)
+# print(len(meu_nome))
+
+# bacon = 20
+# bacon + 1
+# print(bacon)
+print(f'I have eaten {99+1} burritos.')
