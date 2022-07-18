cf15a7c4454507e06baef2ba8118d72d9407f0ab
Carlos Roberto <carlosrjhoe@gmail.com>
2022-07-18 14:00:03 -0300

    Voltando com tudo em python

---
+++ Livro_Automatize_tarefas_maçantes_com_Python/Funçoes/teste.py
@@ -0,0 +1,17 @@
+""" A primeira linha contém uma instrução def u que define uma função
+chamada hello(). O código no bloco após a instrução def v é o corpo da
+função. Esse código é executado quando a função é chamada, e não quando
+ela é inicialmente definida """
+
+# def hello(name):
+#   print(f'Olá, {name}')
+  
+# hello('Carlos')
+# hello('Mayara')
+# hello('Neto')
+# hello('Luna')
+
+""" Valores de retorno e instruções return """
+
+
+
