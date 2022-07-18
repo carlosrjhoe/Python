40a2f7e5493d5b71a7f797d7c689f5da4d426453
Carlos Roberto <carlosrjhoe@gmail.com>
2022-07-17 21:31:29 -0300

    Estudando controle de fluxo

---
+++ Livro_Automatize_tarefas_maçantes_com_Python/Controle_de_fluxo/teste.py
@@ -0,0 +1,44 @@
+# span = True
+# print(span)
+
+# Operadores de comparação
+"""
+    == Igual a
+    != Diferente de
+    < Menor que
+    > Maior que
+    <= Menor ou igual a
+    >= Maior ou igual a 
+"""
+# print(42 == 42)
+# print(42 == '42')
+# print(42 != 42)
+# print(42 != '42')
+# print(42 >= 42)
+
+# Operadores booleanos A tabela verdade do operador "and"
+
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+# Operadores booleanos A tabela verdade do operador "or"
+
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+# Operadores booleanos A tabela verdade do operador "not"
+
+# print(not True)
+# print(not False)
+# print(not True)
+# print(not False)
+
+# Misturando operadores booleanos e de comparação
+
+print((4 < 5) and (5 > 6))
+print((4 < 5) and (9 < 6))
+print((1 == 2) or (2 == 2))
