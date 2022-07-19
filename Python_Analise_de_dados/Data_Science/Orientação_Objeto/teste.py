cd3c35a2bfa6d8b1bc711b5979dc4a1f113b3c28
Carlos Roberto <carlosrjhoe@gmail.com>
2022-07-19 19:18:33 -0300

    Estudando OO

--- Python_Analise_de_dados/Data_Science/Orientação_Objeto/teste.py
+++ Python_Analise_de_dados/Data_Science/Orientação_Objeto/teste.py
@@ -1,14 +1,24 @@
 # Criando uma Classe
-class Livro():
-  def __init__(self):
-    self.titulo = 'O monge e o executivo'
-    self.isbn = 99988877
-    print('Construtor chamado para criar um objeto desta classe.')
+# class Livro():
+#   def __init__(self, titulo, isbn):
+#     self.titulo = titulo
+#     self.isbn = isbn
+#     print('Construtor chamado para criar um objeto desta classe.')
     
-  def imprime(self):
-    print(f'Foi criado o livro {self.titulo} e ISBN é {self.isbn}')
+#   def imprime(self):
+#     print(f'Foi criado o livro {self.titulo} e ISBN é {self.isbn}')
     
-Livro_01 = Livro()
+# Livro_01 = Livro('A menina que roubava livros', 7161514)
 
-print(type(Livro_01))
-Livro_01.imprime()
+# print(type(Livro_01))
+# Livro_01.imprime()
+
+class Cachorro():
+  def __init__(self, raca) -> None:
+    self.raca = raca
+    
+rex = Cachorro(raca='labrador')
+print(rex.raca)
+
+tufik =Cachorro(raca="poddle")
+print(tufik.raca)
