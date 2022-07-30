4810260841863cb31150d9316a90d602512d9f9d
Carlos Roberto <carlosrjhoe@gmail.com>
2022-07-29 21:39:52 -0300

    Estudando selenium

--- Live_Dinoussaou_Python/Live_de_Python_01/test_BDD.py
+++ Live_Dinoussaou_Python/Live_de_Python_01/test_BDD.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.get("https://google.com")
+driver.get("https://www.google.com")
+
+campo = driver.find_element(By.CLASS_NAME, 'gLFyf').send_keys('python')
+# botao = driver.find_element(By.CLASS_NAME, 'gNO89b')
