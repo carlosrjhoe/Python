7d7a368fbb1fe66711bd4a95f624687998702075
Carlos Roberto <carlosrjhoe@gmail.com>
2022-08-02 20:33:58 -0300

    navegando entre os atributos com selenium

--- Curso_Selenium_Eduardo_Mendes/Navegação e Atributos/Aula_04/exemplo.py
+++ Curso_Selenium_Eduardo_Mendes/Navegação e Atributos/Aula_04/exemplo.py
@@ -4,11 +4,10 @@ from selenium.webdriver.common.by import By
 driver = Chrome()
 driver.get('https://selenium.dunossauro.live/aula_04_a.html')
 lista_nao_ordenada = driver.find_elements(By.TAG_NAME, "ul") # 1
+# 1. Buscamos 'ul'
 lis = lista_nao_ordenada.find_elements(By.TAG_NAME, "li") # 2
-# lis[0].find_element_by_tag_name('a') # 3
+# 2. Buscamos todos 'li'
+lis[0].find_element(By. TAG_NAME, "a")
+# 3. No primeiro 'li', buscamos 'a' e pegamos seu text.
 
-"""
-1. Buscamos 'ul'
-2. Buscamos todos 'li'
-3. No primeiro 'li', buscamos 'a' e pegamos seu text.
-"""
+
