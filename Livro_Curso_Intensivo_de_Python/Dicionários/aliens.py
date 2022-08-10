c07543c8e5c1fed98aea32ff3467f01a64d8ea32
Carlos Roberto <carlosrjhoe@gmail.com>
2022-08-10 11:35:02 -0300

    estudando dicionários

---
+++ Livro_Curso_Intensivo_de_Python/Dicionários/aliens.py
@@ -0,0 +1,57 @@
+alien_0 = {'cor': 'verde', 'ponto': 5}
+
+# print(alien_0['cor'])
+# print(alien_0['ponto'])
+
+print(f'Você acabou de ganhar {alien_0["ponto"]} pontos')
+
+
+alien_0['posicao_x'] = 10 # Adicionar nova chave
+
+
+aliens = {} # Criar um dicionário vazio
+print(f'Dicionario vazio {aliens}')
+aliens = {'nome': 'carlos'}
+print(aliens['nome'])
+
+aliens['nome'] = 'Mayara' # Modificando valores em um dicionário
+print(aliens['nome'])
+
+print('###############################################')
+
+alien_1 = {'Nome': 'Neto', 'idade': 6, 'posição': 3}
+
+if alien_1['posição'] == 1:
+    incremento = 0
+elif alien_1['posição'] == 3:
+    incremento = 2
+    
+alien_1['posição'] = alien_1['posição'] + incremento # A nova posição é a posição antiga somada ao incremento
+
+print(f'{alien_1}')
+
+print('###############################################')
+
+"""Removendo pares chave-valor"""
+
+pessoa = {'Nome': 'Luna', 'Idade': 4}
+print(pessoa)
+
+del pessoa['Idade'] # Deletar uma chave de um dicionário
+
+print(pessoa)
+print('###############################################')
+
+"""Um dicionário de objetos semelhantes"""
+
+linguagem_favorita = {
+    'carlos': 'python',
+    'mayara': 'java',
+    'neto': 'c++',
+    'luna': 'javascript'
+}
+
+print(linguagem_favorita)
+print(f'A linguagem favorita de Carlos é {linguagem_favorita["carlos"].title()}')
+
+print('###############################################')
