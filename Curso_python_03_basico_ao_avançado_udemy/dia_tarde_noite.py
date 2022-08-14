99f3624b8b90c6bd0a17fe459b0cfe236368f801
Carlos Roberto <carlosrjhoe@gmail.com>
2022-08-14 09:20:00 -0300

    Estudando tratamento de numeros com python

---
+++ Curso_python_03_basico_ao_avançado_udemy/dia_tarde_noite.py
@@ -0,0 +1,16 @@
+from cgitb import text
+from unicodedata import numeric
+
+
+texto = 'DIA TARDE OU DE NOITE'
+print('#' * len(texto))
+print(texto.center(len(texto)))
+print('#' * len(texto))
+
+numero = input('Digite um numero entre [1-24]: ')
+
+if numero.isdigit():
+    numero = int(numero)
+    if numero <= 12: print('Bom dia!')
+    elif numero <=18: print('Boa tarde!')
+    else: print('Boa noite!')
