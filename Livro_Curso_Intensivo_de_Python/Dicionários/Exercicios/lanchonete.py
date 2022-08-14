e287524aa752be5e6c7edc2d25d1e473b9c6a7e1
Carlos Roberto <carlosrjhoe@gmail.com>
2022-08-14 19:17:22 -0300

    Organizando os arquivos novamente

---
+++ Livro_Curso_Intensivo_de_Python/Dicionários/Exercicios/lanchonete.py
@@ -0,0 +1,12 @@
+"""7.8 – Lanchonete: Crie uma lista chamada sandwich_orders e a preencha com os
+nomes de vários sanduíches. Em seguida, crie uma lista vazia chamada
+finished_sandwiches. Percorra a lista de pedidos de sanduíches com um laço e
+mostre uma mensagem para cada pedido, por exemplo, Preparei seu sanduíche
+de atum. À medida que cada sanduíche for preparado, transfira-o para a lista de
+sanduíches prontos. Depois que todos os sanduíches estiverem prontos, mostre uma
+mensagem que liste cada sanduíche preparado"""
+
+texto = 'LANCHONETE DE NETO E LUNA'
+print('#'*len(texto))
+print(f'{texto.center(len(texto))}')
+print('#'*len(texto))
