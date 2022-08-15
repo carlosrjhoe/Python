b0bcf207abeec2c2cd72e5b9457351eeb10ff879
Carlos Roberto <carlosrjhoe@gmail.com>
2022-08-15 18:40:24 -0300

    Exercicio lanchonete

--- Livro_Curso_Intensivo_de_Python/Dicionários/Exercicios/lanchonete.py
+++ Livro_Curso_Intensivo_de_Python/Dicionários/Exercicios/lanchonete.py
@@ -6,7 +6,27 @@ de atum. À medida que cada sanduíche for preparado, transfira-o para a lista d
 sanduíches prontos. Depois que todos os sanduíches estiverem prontos, mostre uma
 mensagem que liste cada sanduíche preparado"""
 
+from time import sleep
+
 texto = 'LANCHONETE DE NETO E LUNA'
 print('#'*len(texto))
 print(f'{texto.center(len(texto))}')
 print('#'*len(texto))
+
+sanduiches = ['misto quente', 'x-tudo', 'x-burguer', 'x-salada','x-bacon','x-egg','x-frango']
+sanduiche_terminados = []
+
+while sanduiches:
+    sanduiche = sanduiches.pop()
+    print(f'Preparando sanduiche de {sanduiche}')
+    sanduiche_terminados.append(sanduiche)
+    sleep(1)
+
+texto2 = 'LISTA DE PEDIDOS PRONTOS'
+print('#'*len(texto2))
+print(f'{texto2.center(len(texto2))}')
+for chave, pedidos in enumerate(sanduiche_terminados, start=1):
+    print(f'O pedido {chave}: sanduiche de {pedidos}, está pronto!')
+    sleep(1)
+
+    
