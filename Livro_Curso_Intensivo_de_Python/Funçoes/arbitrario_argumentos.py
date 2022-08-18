b43ed2e89f1bb579843a98b8f4aaf8276253e565
Carlos Roberto <carlosrjhoe@gmail.com>
2022-08-17 22:34:12 -0300

    Usando argumentos nomeados arbitrários

--- Livro_Curso_Intensivo_de_Python/Funçoes/arbitrario_argumentos.py
+++ Livro_Curso_Intensivo_de_Python/Funçoes/arbitrario_argumentos.py
@@ -3,3 +3,18 @@ def familia(*nomes):
         print(f'{nome.title()}')
 
 familia('carlos', 'mayara', 'neto', 'luna', 'rose', 'lucia', 'milena', 'emilly')
+
+print('#'*50)
+
+def perfil_construcao(primeiro, segundo, **informacao_usuario):
+    perfil = {}
+    perfil['primeiro_nome'] = primeiro
+    perfil['segundo_nomes'] = segundo
+    for k, v in informacao_usuario.items():
+        perfil[k] = v.title()
+    return perfil
+    
+    
+perfil_princial = perfil_construcao("carlos", "roberto", localizacao="brasil", campo="gilene_di_carle")
+
+print(perfil_princial)
