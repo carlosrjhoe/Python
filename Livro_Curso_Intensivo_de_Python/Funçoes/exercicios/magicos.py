0e57f5678636c4ae5822af092b49a4b2b46d4355
Carlos Roberto <carlosrjhoe@gmail.com>
2022-08-17 22:09:58 -0300

    fazendo exercicios

---
+++ Livro_Curso_Intensivo_de_Python/Funçoes/exercicios/magicos.py
@@ -0,0 +1,9 @@
+magicos_famosos = ['Howard Thurston', 'David Copperfield', 'Lance Burton', 'Grande Houdini', 'David Blaine', 'Dynamo', 'Derren Brown', 'Criss Angel']
+
+def mostrar_magicos(magicos_famosos):
+    for magico in magicos_famosos:
+        print(f'O magico: {magico}')
+        
+    
+    
+mostrar_magicos(magicos_famosos)
--- Livro_Curso_Intensivo_de_Python/Funçoes/pessoas.py
+++ Livro_Curso_Intensivo_de_Python/Funçoes/pessoas.py
@@ -1,6 +1,18 @@
-def pessoas(nomes):
-    for nome in nomes:
-        print(f'Olá {nome.title()}')
+def imprime_moledos(desenhos_nao_impressos, modelos_completados):
+    while desenhos_nao_impressos:
+        design_atual = desenhos_nao_impressos.pop()
+        print(f'Modelo de impressao: {design_atual}')
+        modelos_completados.append(design_atual)
         
-nomes = ['carlos', 'may', 'luna', 'neto']
-pessoas(nomes)
+
+def mostrar_modelos_concluídos(modelos_completados):
+    print(f'Os seguintes modelos foram impressos:')
+    for modelos in modelos_completados:
+        print(f'{modelos}')
+
+
+desenhos_nao_impressos = ['iphone case', 'robot pendant', 'dodecahedron']
+modelos_completados = []
+
+imprime_moledos(desenhos_nao_impressos[:], modelos_completados)
+mostrar_modelos_concluídos(modelos_completados)
