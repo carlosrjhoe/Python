7df5d4d1a4dcdf0a7655656ccf496966a4ba5799
Carlos Roberto <carlosrjhoe@gmail.com>
2022-08-20 19:51:47 -0300

    Estudando dicionarios com OrderedDict

--- Livro_Curso_Intensivo_de_Python/Dicionários/Entrada_usuario/linguagens_favoritas.py
+++ Livro_Curso_Intensivo_de_Python/Dicionários/Entrada_usuario/linguagens_favoritas.py
@@ -7,12 +7,16 @@ print(f'#' * len(texto))
 
 linguagem_favorita = OrderedDict()
 
-linguagem_favorita = {
-    'carlos': ['python'],
-    'mayara': ['java'],
-    'neto': ['c++'],
-    'luna': ['javascript']
-}
+linguagem_favorita['carlos'] = 'python',
+linguagem_favorita['mayara'] = 'java',
+linguagem_favorita['neto'] = 'python',
+linguagem_favorita['luna'] = 'javascript',
+# linguagem_favorita = {
+#     'carlos': ['python'],
+#     'mayara': ['java'],
+#     'neto': ['c++'],
+#     'luna': ['javascript']
+# }
 
 for nome, linguagens in linguagem_favorita.items():
     print(f'Para {nome.title()}, suas linguagens de programação favoritas são:')
