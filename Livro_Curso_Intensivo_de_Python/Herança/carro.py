3d04a452eb4b5c72cccee9064ad687824be6847c
Carlos Roberto <carlosrjhoe@gmail.com>
2022-08-29 11:22:51 -0300

    Organizando arquivos

---
+++ Livro_Curso_Intensivo_de_Python/Herança/carro.py
@@ -0,0 +1,20 @@
+class Carro():
+    
+    def __init__(self, tipo, modelo, ano):
+        self.tipo = tipo
+        self.modelo = modelo 
+        self.ano = ano
+        self.odometro = 1000
+        
+    def descricao_carro(self):
+        tipagem_carro = f'Tipo: {self.tipo.title()}\nModelo: {self.modelo.title()}\nFabricação: {self.ano}'
+        return tipagem_carro
+    
+    def ler_odometro(self):
+        print(f'O carro tem {self.odometro:.2f}km rodados.')
+        
+    def atualizar_odometro(self, kilometragem):
+        if kilometragem >= self.odometro:
+            self.odometro = kilometragem
+        else:
+            print('Você não pode reverter um odômetro!')
