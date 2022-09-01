453d6714405ba0383e53a4a9d9f9b752d5032936
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-01 05:44:00 -0300

    eexercicio 63

--- Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_51_a_80/Exercicio_62/Main.py
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_51_a_80/Exercicio_62/Main.py
@@ -1,17 +1,41 @@
 from medicos import Medicos
+from cadastro_plano_de_saude import usuarios
 
-def consulta():
+texto = 'Exercicio - 63 (Medicos)'
+print('#' * len(texto))
+print(f'{texto.center(len(texto))}')
+print('#' * len(texto))
+
+usuario = input('Digite seu numero de cadastro: ')
+
+if usuario in usuarios.keys():
+    
+    if usuario == '001':
+        usuario = 'carlos'
+        print(f'Bem-vindo {usuario.title()}')
+    elif usuario == '002':
+        usuario = 'mayara'
+        print(f'Bem-vinda {usuario.title()}')
+    elif usuario == '003':
+        usuario = 'neto'
+        print(f'Bem-vindo {usuario.title()}')
+    elif usuario == '004':
+        usuario = 'luna'
+        print(f'Bem-vinda {usuario.title()}')
+        
     menu = input('Deseja marcar uma consulta[S ou N]? ').upper()
-
+    
     if menu == 'N':
         print(f'Ok, Obrigado.')
     else:
-        nome = input('Qual o seu nome: ').title()
         medico = int(input('Gostaria de marcar com qual Médico: \n 1: Maria Rosiclé \n 2: Ana Lúcia\nOpcção: '))
-        
+
         if medico == 1:
-            print(f'Olá, {nome}. \nSua consulta com a Dra {Medicos[0]} foi marcada com sucesso!')
+            print(f'Sua consulta com a Dra {Medicos[0]} foi marcada com sucesso!')
         elif medico == 2:
-            print(f'Olá, {nome}. \nSua consulta com a Dra {Medicos[1]} foi marcada com sucesso!')
-            
-consulta()
+            print(f'Sua consulta com a Dra {Medicos[1]} foi marcada com sucesso!')
+    
+else:
+    print('Usuário desconhecido!')
+        
+        
---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_51_a_80/Exercicio_62/cadastro_plano_de_saude.py
@@ -0,0 +1,6 @@
+usuarios = {
+    '001': 'carlos',
+    '002': 'mayara',
+    '003': 'neto',
+    '004': 'luna'
+}
