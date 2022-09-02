ed4f2d9bbb510afd76650abcf6f7409729e68b9a
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-02 19:40:14 -0300

    exercicio 68

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_51_a_80/Ex68_pessoa.py
@@ -0,0 +1,10 @@
+class Pessoa:
+
+    def __init__(self, nome, idade, altura, peso, sexo):
+        self.nome = None
+        self.idade = None
+        self.altura = None
+        self.peso = None
+        self.sexo = None
+        
+        
