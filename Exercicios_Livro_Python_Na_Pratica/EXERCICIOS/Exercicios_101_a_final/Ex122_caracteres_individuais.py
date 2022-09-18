381bbad6f73bd2c112cb94f57fce1ee649b19f2c
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-18 19:53:23 -0300

    resolução do exercicio 122

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_101_a_final/Ex122_caracteres_individuais.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+
+class Verificar_letras:
+    
+    def __init__(self) -> None:
+        texto = 'Programa irá verificar a quantidade das vogais'
+        print(f'{"#"*len(texto)}')
+        print(f'{texto.center(len(texto))}')
+        print(f'{"#"*len(texto)}')
+        
+        self.texto_a_verificar = 'A radiologia e a ciencia que estuda a anatomia por meio de radiacoes'
+        
+    def exibir_informacoes(self):
+        a1,a2,a3,a4,a5,a6 = Counter(self.texto_a_verificar).most_common(6)
+        
+        print(f'Texto:\n{self.texto_a_verificar}')
+        print(f'O elemento mais comum é: {a2[0]}, se repetem {a2[1]} vezes')
+        print(f'O elemento mais comum é: {a3[0]}, se repetem {a3[1]} vezes')
+        print(f'O elemento mais comum é: {a4[0]}, se repetem {a4[1]} vezes')
+        print(f'O elemento mais comum é: {a5[0]}, se repetem {a5[1]} vezes')
+        print(f'O elemento mais comum é: {a6[0]}, se repetem {a6[1]} vezes')
