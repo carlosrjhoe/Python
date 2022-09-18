b925a19987c87219ec1890b84639916ca209a191
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-18 11:39:01 -0300

    Resolução do exercicio 104

--- Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_101_a_final/Ex104_combinacoes.py
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_101_a_final/Ex104_combinacoes.py
@@ -1,7 +1,7 @@
-class Gerador:
+class Combinacoes:
     
     def __init__(self) -> None:
-        self.texto = 'Programa que vai gerar uma lista'
+        self.texto = 'Programa que vai gerar possiveis combinações entre numeros'
         print(f'{"#"*len(self.texto)}')
         print(f'{self.texto.center(len(self.texto))}')
         print(f'{"#"*len(self.texto)}')
@@ -10,11 +10,13 @@ class Gerador:
         self.lista_2 = []
         
     def gerar_lista(self):
-        for i in range(51):
-            self.lista_1.append(i)
+        for i in [1,2,3]:
+            for j in [4,5,6]:
+                if i != j:
+                    self.lista_1.append((i,j))
             
         # Um for com listComprehension
-        self.lista_2 = [for j in range(51)]
+        self.lista_2 = [(i,j) for i in[1,2,3] for j in [4,5,6] if i != j]
             
     def exibir_lista(self):
         print(f'{self.lista_1}')
