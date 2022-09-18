6ce5944ceee5e8643ad4df5b8aeb8a5a79811ce1
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-18 07:55:53 -0300

    Modulo para execultar os exercicios

--- Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_81_a_100/exemplo.py
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS/Exercicios_81_a_100/exemplo.py
@@ -1,10 +1,5 @@
-from Ex99_classe import Carro
+from Ex100_usuarios import Jogos
 
-carlos = Carro('Audi', 'A3',2006,'Preto',19.000)
-mayara = Carro('Wolkswagem', 'Gol',2010,'Prata',22.000)
-neto = Carro('Ford', 'Focus',2003,'Branco',9.000)
-
-carlos.cabecalho()
-carlos.descricao()
-mayara.descricao()
-neto.descricao()
+carlos = Jogos()
+carlos.perguntas()
+carlos.maior_de_tres()
