ced122f72f2ad1c4d7dd935a7987d45118c2c296
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-20 21:01:25 -0300

    Estudando livro python da casa do codigo

---
+++ Livro_Python_casa_do_Codigo/Estrutura_de_Dados/Dicionarios/exemplo.py
@@ -0,0 +1,9 @@
+def experiência_de_descompactação(*args):
+    args1 = args[0]
+    args2 = args[1]
+    outros = args[:4]
+    print(args1)
+    print(args2)
+    print(outros)
+    
+experiência_de_descompactação(1,2,3,4,5)
