73aa88f5917517bd181bfc9f3942cf493282c703
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-22 17:22:37 -0300

    Estudando a função map de python

--- Curso_python_03_basico_ao_avançado_udemy/Seção_3_Programação procedural/mapeamento.py
+++ Curso_python_03_basico_ao_avançado_udemy/Seção_3_Programação procedural/mapeamento.py
@@ -2,9 +2,12 @@ from dados import lista_de_precos, lista_de_pessoas, lista_de_numeros
 
 # MAP
 # nova_lista = map(lambda x : x * 2, lista_de_numeros)
+# print(lista_de_numeros)
+# print(list(nova_lista))
 
 # LISTACONVERRANGE
 # nova_lista = [x * 2 for x in lista_de_numeros]
+# print(nova_lista)
 
 # print(lista_de_numeros)
 # print(list(nova_lista))
@@ -13,10 +16,16 @@ from dados import lista_de_precos, lista_de_pessoas, lista_de_numeros
 #     p['preco'] = round(p['preco'] * 1.05, 2)
 #     return p
 
+
 # novos_produtos = map(aumento_de_5_porcento, lista_de_precos)
 # for produto in novos_produtos:
 #     print(produto)
 
-nomes = map(lambda pessoa: pessoa['nome'], lista_de_pessoas)
-for nome in nomes:
-    print(f'{nome.title()}')
+
+def aumentar_idade(p):
+    p['nova_idade'] = round(p['idade'] * 1.2)
+    return p
+    
+pessoas = map(aumentar_idade, lista_de_pessoas)
+for pessoa in pessoas:
+    print(f'Meu nome é: {pessoa["nome"].title()}, e eu tenho {pessoa["idade"]} anos de idade.')
