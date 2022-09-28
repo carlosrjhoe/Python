02fd94191bcd3c5a1c9a772f09d062398d19633e
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-28 20:35:04 -0300

    Estudando POO, e melhorando as linhas de código

--- Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/Pessoa.py
+++ Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/Pessoa.py
@@ -1,17 +1,41 @@
+from datetime import datetime
+
 class Pessoa:
-    def __init__(self, nome, idade, comendo=False, falando=False):
-        self.nome = nome
-        self.idade = idade
-        self.comendo = comendo
+    data = datetime.now()
     
+    def __init__(self, nome, nascimento, comendo=False, falar=False):
+        self.nome = nome
+        self.nascimento = nascimento
+        self.comendo = comendo
+        self.falar = falar
+    
+    def get_idade(self):
+        self.idade = int(self.data.year - self.nascimento)
+        return self.idade
+    
+    def falando(self, assunto):
+        if self.comendo:
+            print(f'{self.nome.title()} Está comendo, não pode falar de boca cheia.')
+            return
+        
+        print(f'{assunto}')
+        self.comendo = False
+        
+    def informacao(self):
+        print(f'{self.nome.title()} nasceu em {self.nascimento}, e tem {self.get_idade()} anos.')
     
     def comer(self, alimento):
         if self.comendo:
             print(f'{self.nome.title()} já está comendo.\n')
             return
         
-        print(f'{self.nome.title()} está comendo {alimento}')
+        print(f'{self.nome.title()} está comendo {alimento}.')
         self.comendo = True
         
+    def para_de_comer(self):
+        if not self.comendo:
+            print(f'{self.nome.title()} Não está comendo.')
+            return
         
-   
+        print(f'{self.nome.title()} Parou de comer.')
+        self.comendo = False
--- Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/main.py
+++ Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/main.py
@@ -1,9 +1,10 @@
 from Pessoa import Pessoa
 
-carlos = Pessoa('carlos', 36)
-carlos.informacao()
+carlos = Pessoa('carlos',1990)
 carlos.comer('Frutas')
+carlos.falando('olá, tudo bem? Gostaria de provar?')
+carlos.para_de_comer()
+carlos.falando('Olá, tudo bem? Gostaria de provar?')
 
-mayara = Pessoa('mayara', 37)
-mayara.informacao()
-mayara.comer('Pão')
+
+
