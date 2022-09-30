c0254e0bcc2bd17390a7371ac1dd4c79c0bb7047
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-30 10:28:50 -0300

    Reforçando os conceitos de property

---
+++ Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/Get_Set/Reforço_get_e_set.py
@@ -0,0 +1,19 @@
+# SETTER = CONFIGURAR UM VALOR (=)
+# GETTER = OBTER (RETORNAR) O VALOR QUE FOI CONFIGURADO NO SETTER
+
+class Pessoa:
+    def __init__(self, nome):
+        self.nome = nome
+    
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        print('Foi executado!')
+        self._nome = nome
+    
+    
+pessoa_01 = Pessoa('Carlos')
+print(pessoa_01.nome)
