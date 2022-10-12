93fb320d4974acef4892884fe65af92e0b547807
Carlos Roberto <carlosrjhoe@gmail.com>
2022-10-12 09:41:39 -0300

    Atualizando arquivos

--- Curso_python_03_basico_ao_avançado_udemy/Banco_de_Dados_SQLite/venv/main.py
+++ Curso_python_03_basico_ao_avançado_udemy/Banco_de_Dados_SQLite/venv/main.py
@@ -1,18 +1,27 @@
 import pymysql.cursors
+from contextlib import contextmanager
 
-conexao = pymysql.connect(
-    host='127.0.0.1',
-    user='root',
-    password='',
-    db='clientes',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor
-)
+@contextmanager
+def conecta():
+    conexao = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='',
+        db='clientes',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    try:
+        yield conexao
+    finally:
+        conexao.close()
 
-with conexao.cursor() as cursor:
-    cursor.execute('SELECT * FROM clientes LIMIT 50')
-    resultado = cursor.fetchall()
-    for linha in resultado:
-        print(f'{linha}')
+
+with conecta() as conexao:
+    with conexao.cursor() as cursor:
+        cursor.execute('SELECT * FROM clientes LIMIT 50')
+        resultado = cursor.fetchall()
+        for linha in resultado:
+            print(f'{linha}')
     
 conexao.close()
