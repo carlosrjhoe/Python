4e3d9fd0026051c3a83c44c494818cbfb99188f2
Carlos Roberto <carlosrjhoe@gmail.com>
2022-10-15 13:58:49 -0300

    Criando um novo app

--- Curso_python_03_basico_ao_avançado_udemy/Django/Primeiro_projeto/django/Meu_site/settings.py
+++ Curso_python_03_basico_ao_avançado_udemy/Django/Primeiro_projeto/django/Meu_site/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+# Registrar apps
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'sobre.apps.SobreConfig',
 ]
 
 MIDDLEWARE = [
