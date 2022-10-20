4dac06953385e61e851d74dbd22502ebc5f41226
Carlos Roberto <carlosrjhoe@gmail.com>
2022-10-20 04:59:32 -0300

    Adicionando uma urls dinamicamente

--- Curso_python_03_basico_ao_avançado_udemy/Django/Segundo_projeto/django/paginas/urls.py
+++ Curso_python_03_basico_ao_avançado_udemy/Django/Segundo_projeto/django/paginas/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # (name='index') -> Serve para adicionar dinamicamente uma url
     path('sobre/', views.sobre, name='sobre'),
 ]
