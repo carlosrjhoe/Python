30cf9c15c53263224f89323aaeee353b2a9296cd
Carlos Roberto <carlosrjhoe@gmail.com>
2022-10-22 05:08:17 -0300

    Criando um admin, e configurando o models

--- Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/admin.py
+++ Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
 # Register your models here.
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'date_criacao', 'categoria')
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/models.py
+++ Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/models.py
@@ -6,7 +6,10 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length = 255)
 
-
+    def __str__(self):
+        return self.nome
+    
+    
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255, blank=True)
@@ -16,5 +19,8 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return self.nome
+    
 
     
