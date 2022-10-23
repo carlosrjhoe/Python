21f7c7830855901508797ea0f8efdd5150d3aec6
Carlos Roberto <carlosrjhoe@gmail.com>
2022-10-23 09:40:43 -0300

    Adicionando filter no admin

--- Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/admin.py
+++ Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/admin.py
@@ -4,6 +4,9 @@ from .models import Categoria, Contato
 # Register your models here.
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome', 'telefone', 'date_criacao', 'categoria')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('nome', 'sobrenome')
+    list_per_page = 5
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
