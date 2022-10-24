57aef57c82a999836533ab155ba1d9180eee0321
Carlos Roberto <carlosrjhoe@gmail.com>
2022-10-24 19:15:47 -0300

    Editando arquivo admin.py

--- Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/admin.py
+++ Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/admin.py
@@ -5,8 +5,8 @@ from .models import Categoria, Contato
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome', 'telefone', 'date_criacao', 'categoria')
     list_display_links = ('id', 'nome', 'sobrenome')
-    list_filter = ('nome', 'sobrenome')
-    list_per_page = 5
+    list_filter = ('nome', 'categoria')
+    list_per_page = 10
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
