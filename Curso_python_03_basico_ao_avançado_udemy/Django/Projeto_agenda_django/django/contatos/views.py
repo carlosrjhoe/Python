b343a14973c08d826638e46b8b10cdd58a28a833
Carlos Roberto <carlosrjhoe@gmail.com>
2022-10-27 20:56:41 -0300

    Ordenando e filtrando valores

--- Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/views.py
+++ Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('id').filter()
     paginator = Paginator(contatos, 5)
     
     paginas = request.GET.get('paginas')
@@ -17,6 +17,10 @@ def index(request):
 def ver_contato(request, contato_id):
     # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+    
+    # if not contato.mostrar:
+    #     raise Http404()
+    
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
