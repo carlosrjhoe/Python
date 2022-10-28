46aae0e6be9319653bab141808744b7b5f26fc20
Carlos Roberto <carlosrjhoe@gmail.com>
2022-10-28 20:41:33 -0300

    Adicionando um campo de busca pelo nome e sobre nome

--- Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/views.py
+++ Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_agenda_django/django/contatos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -15,6 +16,7 @@ def index(request):
     })
 
 def ver_contato(request, contato_id):
+    """Responsável por mostrar os contatos"""
     # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
     
@@ -23,4 +25,20 @@ def ver_contato(request, contato_id):
     
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+    
+def busca(request):
+    """Responsável por fazer uma busca pelo nome e sobrenome"""
+    termo = request.GET.get('termo')
+    contatos = Contato.objects.order_by('id').filter(
+        Q(nome__icontains=termo) | Q(sobrenome__icontains=termo),
+        mostrar=True
+    )
+    
+    paginator = Paginator(contatos, 5)
+    paginas = request.GET.get('paginas')
+    contatos = paginator.get_page(paginas)
+    
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
