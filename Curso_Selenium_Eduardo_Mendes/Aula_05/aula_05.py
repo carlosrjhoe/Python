9b4044ac5829648ebcac9c9e9ca256e47cc85721
Carlos Roberto <carlosrjhoe@gmail.com>
2022-10-29 14:07:43 -0300

    Estudando selenium

--- Curso_Selenium_Eduardo_Mendes/Aula_05/aula_05.py
+++ Curso_Selenium_Eduardo_Mendes/Aula_05/aula_05.py
@@ -1,6 +1,10 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 
-url = 'http://selenium.dunossauro.live/aula_05_a.html'
+url = "https://www.hashtagtreinamentos.com/curso-python"
 driver = Chrome()
 driver.get(url)
-div_1 = driver.find_element_by_id()
+
+driver.find_element('xpath', '//*[@id="_form_1919_"]/div[1]/div[1]/div/input').send_keys('Carlos Roberto')
+driver.find_element('xpath', '//*[@id="_form_1919_"]/div[1]/div[2]/div/input').send_keys('carlosrjhoe@gmail.com')
+driver.find_element('xpath', '//*[@id="_form_1919_submit"]').click()
