7576c37dc51f602b0d4d8d0fd86e180b2facb5c3
Carlos Roberto <carlosrjhoe@gmail.com>
2022-11-23 19:53:26 -0300

    Estudando lista

--- Exercicios_livro_Intesivo_Python/Fatiando_listas/percorrer_lista_fatiada.py
+++ Exercicios_livro_Intesivo_Python/Fatiando_listas/percorrer_lista_fatiada.py
@@ -3,3 +3,8 @@ jogadores = ['charles', 'martina', 'michael', 'florence', 'eli']
 print(f'Here are the first three players on my team:')
 for jogador in jogadores[:3]:
     print(f'{jogador.title()}')
+
+
+
+novos_jogadores = jogadores[:]
+print(novos_jogadores)
