83262e9cfc6d01c00139628a03540208477fafec
Carlos Roberto <carlosrjhoe@gmail.com>
2022-11-24 05:28:16 -0300

    Estudando mais sobre fatiamentos de listas

--- Exercicios_livro_Intesivo_Python/Fatiando_listas/percorrer_lista_fatiada.py
+++ Exercicios_livro_Intesivo_Python/Fatiando_listas/percorrer_lista_fatiada.py
@@ -1,10 +1,9 @@
-jogadores = ['charles', 'martina', 'michael', 'florence', 'eli']
+jogadores = ['charles', 'martina', 'michael', 'florence', 'eli', 'carlos', 'mayara', 'alice', 'neto', 'luna']
 
-print(f'Here are the first three players on my team:')
-for jogador in jogadores[:3]:
-    print(f'{jogador.title()}')
+print(f'{jogadores}')
+print(f'Os três primeiros da lis são: {jogadores[:3]}')
+print(f'Três itens do meio da lista são: {jogadores[3:6]}')
+print(f'Os três últimos itens da lista são:: {jogadores[7:]}')
 
-
-
-novos_jogadores = jogadores[:]
-print(novos_jogadores)
+for nome in jogadores:
+    print(f'{nome.title()}')
