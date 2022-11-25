b5c0caddfdeea6bcf4ebc736ac38c4338785099c
Carlos Roberto <carlosrjhoe@gmail.com>
2022-11-25 04:39:41 -0300

    Estudando mais sobre tuplas

---
+++ Exercicios_livro_Intesivo_Python/Tuplas/buffet.py
@@ -0,0 +1,13 @@
+cardapio = ('lasanha', 'frango parmegiana', 'carne parmegiana', 'feijoada', 'dobradinha', 'panqueca', 'torta salgada', 'galinha cabidela')
+print('Cardápio antigo')
+# cardapio[5] = 'frango'
+for pratos in cardapio:
+    print(f'{pratos.title()}')
+
+cardapio = ('lasanha', 'frango parmegiana', 'carne parmegiana', 'macarronada', 'dobradinha', 'almôndega', 'torta salgada', 'galinha cabidela')
+
+print('\nCardápio atualizado')
+# cardapio[5] = 'frango'
+for pratos in cardapio:
+    print(f'{pratos.title()}')
+
--- Exercicios_livro_Intesivo_Python/Tuplas/dimensao.py
+++ Exercicios_livro_Intesivo_Python/Tuplas/dimensao.py
@@ -3,9 +3,17 @@ que não podem mudar como imutáveis, e uma lista imutável é chamada de
 tupla"""
 
 dimensao = (300, 50)
+print(f'Tupla original')
+for dados in dimensao:
+    print(f'{dados}', end=' ')
+
+
+dimensao = (200, 150)
+print(f'\nTupla modificada')
+for dados in dimensao:
+    print(f'{dados}', end=' ')
+    
 # dimensao[0] = 100 # objeto não suporta atribuição de item
 # print(f'{dimensao[0]}')
 # print(f'{dimensao[1]}')
 
-for dados in dimensao:
-    print(f'{dados}', end=' ')
