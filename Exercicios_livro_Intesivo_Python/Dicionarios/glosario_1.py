f236b0184c52c79d62b399f17ce5e33310c53a51
Carlos Roberto <carlosrjhoe@gmail.com>
2022-11-27 06:20:21 -0300

    Estudando dicionários com python

--- Exercicios_livro_Intesivo_Python/Dicionarios/glosario_1.py
+++ Exercicios_livro_Intesivo_Python/Dicionarios/glosario_1.py
@@ -5,6 +5,13 @@ linguagem_de_programacao = {
     'luna': 'go',
 }
 
+nomes_escolhidos = ['carlos', 'neto']
+
 for key in linguagem_de_programacao.keys():
     """O método keys() é conveniente quando não precisamos trabalhar com todos os valores de um dicionário. """
-    print(f'Linguagem de programação: {key.title()}\t')
+    print(f'Nome: {key.title()}\t')
+
+    if key in nomes_escolhidos:
+        """No laço, exibimos o nome de cada pessoa. Então, verificamos se o nome com que estamos trabalhando (key) está na lista nomes_escolhidos."""
+        print(f'Olá {key.title()}, Vi que sua linguagem de programação favoria é: {linguagem_de_programacao[key].title()}')
+    
