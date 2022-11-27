2ee6df0c6e76ebb322a6c6e62860bbbd8645c8a0
Carlos Roberto <carlosrjhoe@gmail.com>
2022-11-27 10:33:25 -0300

    Estudando iteração com dicionarios

--- Exercicios_livro_Intesivo_Python/Dicionario_em_dicionarios/main.py
+++ Exercicios_livro_Intesivo_Python/Dicionario_em_dicionarios/main.py
@@ -2,13 +2,13 @@ from usuarios import usuarios
 from datetime import datetime
 ano = datetime.now().year
 
-
 for kye, lista in usuarios.items():
-    print(f'\nDados de {kye.title()}:')
-    print(f'\tNome: {lista["primeiro_nome"].title()}')
-    print(f'\tSobrenome: {lista["segundo_nome"].title()}')
-    print(f'\tNascimento: {lista["nascimento"]}')
-    print(f'\tIdade: {ano - int(lista["nascimento"])}')
-    print(f'\tCidade natal: {lista["cidade"].title()}')
+    if lista['primeiro_nome'] == 'luna' and lista['segundo_nome'] == 'ramos':
+        print(f'\nDados de {kye.title()}:')
+        print(f'\tNome: {lista["primeiro_nome"].title()}')
+        print(f'\tSobrenome: {lista["segundo_nome"].title()}')
+        print(f'\tNascimento: {lista["nascimento"]}')
+        print(f'\tIdade: {ano - int(lista["nascimento"])}')
+        print(f'\tCidade natal: {lista["cidade"].title()}')
     
     
