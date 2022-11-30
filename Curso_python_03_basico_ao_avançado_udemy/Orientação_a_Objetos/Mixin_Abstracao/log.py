30796cc42ddcd2573a45247b117f07ff6d5baca2
Carlos Roberto <carlosrjhoe@gmail.com>
2022-11-30 20:52:55 -0300

    POO avançado

--- Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/Mixin_Abstracao/log.py
+++ Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/Mixin_Abstracao/log.py
@@ -1,14 +1,16 @@
 # Abstração
 class Log:
-    def log(self, msg):
+    def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
 
+    def log_error(self, msg):
+        self._log(f'Error: {msg}')
 
 class LogFileMixin(Log):
-    def log(self, msg):
-        print(msg)
+    def _log(self, msg):
+        print(f'{msg} - {self.__class__.__name__}')
 
 
 if __name__ == '__main__':
     l = LogFileMixin()
-    l.log('Olá, bom dia..')
+    l._log('Olá, bom dia..')
