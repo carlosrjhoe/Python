6ff01778df502a7d532d9775bbe6eafc4548e470
Carlos Roberto <carlosrjhoe@gmail.com>
2022-12-05 05:35:50 -0300

    Modificando o código printing

---
+++ Exercicios_livro_Intesivo_Python/Valores_de_retorno/main_printing.py
@@ -0,0 +1,7 @@
+from printing_models import print_models, show_completed_models
+
+unprinted_designs = ["goku", "vegeta", "picolo", "curirin", "trunks", "gohan", "cell", "freezar"]
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
--- Exercicios_livro_Intesivo_Python/Valores_de_retorno/printing_models.py
+++ Exercicios_livro_Intesivo_Python/Valores_de_retorno/printing_models.py
@@ -1,17 +1,17 @@
 from time import sleep
 
-unprinted_designs = ['dragon ball', 'fly', 'jaspion', 'bleach', 'hunter x hunter', 'demon slayer']
-completed_models = []
 
-while unprinted_designs:
-    current_design = unprinted_designs.pop()
+def print_models(unprinted_designs, completed_models):
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
 
-    print(f"Printing models: {current_design.title()}")
-    completed_models.append(current_design)
-    sleep(1)
+        print(f"Printing models: {current_design.title()}")
+        completed_models.append(current_design)
+        sleep(1)
 
-if __name__ == '__main__':
+def show_completed_models(completed_models):
     print("\nThe following models have been printed")
     for model in completed_models:
         print(f"{model.title()}")
         sleep(1)
+
