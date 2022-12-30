4ab23a2e2ffb017d36618dfeea012dcc8ed37391
Carlos Roberto <carlosrjhoe@gmail.com>
2022-12-30 13:41:00 -0300

    Estudando python

--- Exercicios_Livro_Python_Na_Pratica/EXERCICIOS_RESOLVIDOS_COM_POO/Exercicio_23.py
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS_RESOLVIDOS_COM_POO/Exercicio_23.py
@@ -1,8 +1,9 @@
 num1 = 102
-lista1 = [10, 100, 1000, 10000, 100000]
+lista1 = [1, 10, 100, 1000, 10000, 100000]
 
 def mostra_lista(lista):
-    print(f"{num1 in lista}")
+    for numero in lista:
+        print(f"{numero}")
 
 
 if __name__ == '__main__':
