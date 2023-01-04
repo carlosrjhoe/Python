1870f43491a5fb77e4360e6e88c2ab082508131c
Carlos Roberto <carlosrjhoe@gmail.com>
2023-01-03 21:51:55 -0300

    Plotando dados em um gráfico de temperatura

--- Exercicios_livro_Intesivo_Python/Fazendo_download_de_dados/highs_lows.py
+++ Exercicios_livro_Intesivo_Python/Fazendo_download_de_dados/highs_lows.py
@@ -1,13 +1,25 @@
 import csv
+from matplotlib import pyplot as plt
 
 file_name = "sitka_weather_07-2014.csv"
 with open(file_name) as file:
     reader = csv.reader(file)
     header_row = next(reader)
-    # print(header_row)
 
-    temperatura_maxima = []
+    temperatura_maxima_em_celsius = []
     for i in reader:
-        temperatura_maxima.append(i[1])
+        temp_highs = int(i[1])
 
-    print(temperatura_maxima, end="")
+        # Transforma temperatura fahrenheit em celsius
+        temperatura_maxima_em_celsius.append((temp_highs-32)*5/9)
+
+    # Faz a plotagem dos dados
+    fig = plt.figure(dpi=128, figsize=(10, 6))
+    plt.plot(temperatura_maxima_em_celsius, c="red")
+
+    # Formatar o grafico
+    plt.title("Altas temperaturas diárias, julho de 2014", fontsize=24)
+    plt.xlabel("", fontsize=16)
+    plt.tick_params(axis="both", which="major", labelsize=16)
+    plt.show()
+    
