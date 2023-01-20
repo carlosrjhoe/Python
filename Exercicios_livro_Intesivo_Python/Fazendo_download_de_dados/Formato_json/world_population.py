ca566ae0b5c3deb4727a647944a2cfb3344cad88
Carlos Roberto <carlosrjhoe@gmail.com>
2023-01-20 05:32:14 -0300

    Estudando leitura de arquivo tipo: JSON

---
+++ Exercicios_livro_Intesivo_Python/Fazendo_download_de_dados/Formato_json/world_population.py
@@ -0,0 +1,13 @@
+import json
+
+# Carrega os dados em uma lista
+file_name = 'population_data.json'
+with open(file_name) as file:
+    pop_data = json.load(file)
+
+# Exibe a população de cada país em 2010
+for dict_pop in pop_data:
+    if dict_pop['Year'] == '2010':
+        cidade = dict_pop['Country Name']
+        populacao = dict_pop['Value']
+        print(f'Cidade: {cidade}, População: {populacao} pessoas.')
--- Exercicios_livro_Intesivo_Python/Fazendo_download_de_dados/date_time.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-first_date = datetime.strptime("2022-01-04", "%Y-%m-%d")
-
-print(f"{first_date}")
