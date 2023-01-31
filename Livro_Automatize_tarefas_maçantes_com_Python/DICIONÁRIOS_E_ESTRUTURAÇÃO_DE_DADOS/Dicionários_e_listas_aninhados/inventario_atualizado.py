7d5c23ba539201407791f56e7bde4429c44060cf
Carlos Roberto <carlosrjhoe@gmail.com>
2023-01-31 20:51:26 -0300

    Resolvido 1° parte de exercicio

--- Livro_Automatize_tarefas_maçantes_com_Python/DICIONÁRIOS_E_ESTRUTURAÇÃO_DE_DADOS/Dicionários_e_listas_aninhados/inventario_atualizado.py
+++ Livro_Automatize_tarefas_maçantes_com_Python/DICIONÁRIOS_E_ESTRUTURAÇÃO_DE_DADOS/Dicionários_e_listas_aninhados/inventario_atualizado.py
@@ -1,16 +1,17 @@
-inventario = {'gold coin': 42, 'rope': 1,}
+inventario = {'gold coin': 42, 'rope': 1, 'ruby': 1, 'dagger': 1}
 saqueDragao = ['gold coin', 
                'dagger', 
                'gold coin', 
                'gold coin', 
             ]
 
-def mostra(inventario):
-    total = 0
-    print("Inventario:")
-    for i, j in inventario.items():
-        total = total + j
-    print(total)
+def info(inventario, saqueDragao):
+    print("Inventário:")
+    quantidade = 0
+    for nome, qtd in inventario.items():
+        print(f"{qtd} - {nome}")
+        quantidade = quantidade + qtd
+    print(f"Número total de itens: {quantidade}")
 
-if __name__ == "__main__":
-    mostra(inventario)
+if __name__ == '__main__':
+    info(inventario, saqueDragao)
