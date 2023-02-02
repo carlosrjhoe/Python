8dc294883e6d0f7c5a9279e211073a17f81b7a4c
Carlos Roberto <carlosrjhoe@gmail.com>
2023-02-02 04:25:19 -0300

    Melhorando script de padrões de text

--- Livro_Automatize_tarefas_maçantes_com_Python/Manipulação_de_strings/isPhoneNumber.py
+++ Livro_Automatize_tarefas_maçantes_com_Python/Manipulação_de_strings/isPhoneNumber.py
@@ -17,7 +17,14 @@ def isPhoneNumber(text):
     return True
 
 if __name__ == '__main__':
-    print("415-555-4242 is a phone number:")
-    print(isPhoneNumber("415-555-4242"))
-    print("Moshi moshi is a phone number:")
-    print(isPhoneNumber("Moshi moshi"))
+    # print("415-555-4242 is a phone number:")
+    # print(isPhoneNumber("415-555-4242"))
+    # print("Moshi moshi is a phone number:")
+    # print(isPhoneNumber("Moshi moshi"))
+
+    msg = "Call me at 415-555-1011 tomorrow. 415-555-9999 is my office."
+    for i in range(len(msg)):
+        chuck = msg[i:i + 12]
+        if isPhoneNumber(chuck):
+            print(f"Phone number found: {chuck}")
+    print("Done")
