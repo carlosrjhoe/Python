d9f4f175cddaa5652cd4f9310b9a437b888b9466
Carlos Roberto <carlosrjhoe@gmail.com>
2023-02-21 10:53:05 -0300

    Estudando livro Python Orientação a Objetos

---
+++ Livro_Pytyhon_e_Orientacao_a_Objeto_Caelum/Dicionarios/dicionarios.py
@@ -0,0 +1,15 @@
+carlos = {'nome': 'carlos', 'idade': 37, 'formacao': ['Analista de Sistemas', 'Técnico em mecânica']}
+
+# print(type(carlos))
+# print(len(carlos))
+
+"""Consultando chaves/valores de um dicionário"""
+# print(carlos.get('nome'))
+# print(carlos.get('idade'))
+# print(carlos.get('formacao'))
+
+"""Consultando as chaves de um dicionário"""
+print(carlos.keys())
+
+"""Consultando as values de um dicionário"""
+print(carlos.values())
