96a7a4f1ad5c5431098cddf559b944926d502ff6
Carlos Roberto <carlosrjhoe@gmail.com>
2023-02-20 23:24:35 -0300

    iniciando o selenium IDE

---
+++ Curso_Selenium_Eduardo_Mendes/teste.py
@@ -0,0 +1,8 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+driver = Chrome()
+driver.get('https://ddg.gg')
+
