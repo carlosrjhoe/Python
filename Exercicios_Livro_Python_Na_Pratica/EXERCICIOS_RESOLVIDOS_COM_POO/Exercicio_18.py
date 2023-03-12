d68a26139883cbea32ba3eee4ca8467f7d7b0e50
Carlos Roberto <carlosrjhoe@gmail.com>
2023-03-11 22:33:28 -0300

    Estudando python

---
+++ Exercicios_Livro_Python_Na_Pratica/EXERCICIOS_RESOLVIDOS_COM_POO/Exercicio_18.py
@@ -0,0 +1,11 @@
+def verificar_se_sao_iguais():
+    num1 = 100
+    num2 = 101
+
+    if num1 == num2:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    print(verificar_se_sao_iguais())
--- Start_Capgemini/repita_ate.py
+++ Start_Capgemini/repita_ate.py
@@ -1,3 +1,10 @@
-print('Nossas opções:')
-print('1: Troca de óleo.\n2: Balanceamento.\n3: Sair\n')
-opcao = input('Digite sua opção: ')
+# idades = []
+
+# for i in range(5):
+#     inf_idade = input('Informe sua idade:')
+#     idades.append(int(inf_idade))
+# print(idades)
+# print(len(idades))
+# print(max(idades))
+# print(min(idades))
+
