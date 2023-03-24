5a4d08372e1403dc7d46b00c52ebd5e85e980d5a
Carlos Roberto <carlosrjhoe@gmail.com>
2023-03-23 21:08:39 -0300

    Estudando API com python

--- Livro_Curso_Intensivo_de_Python/Trabalhando_com_API/teste_api.py
+++ Livro_Curso_Intensivo_de_Python/Trabalhando_com_API/teste_api.py
@@ -3,16 +3,20 @@ import requests
 # Faz uma chamada de API e armazena a resposta
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 resp = requests.get(url)
+
+# Um código de status igual a 200 indica sucesso na resposta.) 
 print(f'Status code: {resp.status_code}')
 
 # Armazena a resposta da API em uma variável
 response_dict = resp.json()
 repositorios = response_dict['items']
+
+# Analisa o primeiro repositório
 repo = repositorios[0]
 
 # Processa o resultado
 # print(f'{response_dict.keys()}')
-print(f'Total repositorios: {response_dict["total_count"]}')
+# print(f'Total repositorios: {response_dict["total_count"]}')
 # print(f'Keys: {len(repo)}')
 # for key in sorted(repo.keys()):
 #     print(f'{key}')
@@ -24,7 +28,5 @@ for i in repositorios:
     print(f'Owner: {i["owner"]["login"]}')
     print(f'Stars: {i["stargazers_count"]}')
     print(f'Repository: {i["html_url"]}')
-    print(f'Created: {i["created_at"]}')
-    print(f'Updated: {i["updated_at"]}')
     print(f'Description: {i["description"]}')
     print(f'########################################################################################')
