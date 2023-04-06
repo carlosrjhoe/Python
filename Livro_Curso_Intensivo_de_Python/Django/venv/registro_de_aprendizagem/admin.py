2c99b37561b28002e2fbc190aed9504c0c73812c
Carlos Roberto <carlosrjhoe@gmail.com>
2023-04-05 21:08:26 -0300

    Estudando Django

--- Livro_Curso_Intensivo_de_Python/Django/venv/registro_de_aprendizagem/admin.py
+++ Livro_Curso_Intensivo_de_Python/Django/venv/registro_de_aprendizagem/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from registro_de_aprendizagem.models import Topico, Entrada
 
 # Register your models here.
+admin.site.register(Topico)
+admin.site.register(Entrada)
--- Livro_Curso_Intensivo_de_Python/Django/venv/registro_de_aprendizagem/models.py
+++ Livro_Curso_Intensivo_de_Python/Django/venv/registro_de_aprendizagem/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class topico(models.Model):
+class Topico(models.Model):
     """Um assunto sobre o qual o usuário está aprendendo."""
-
     texto = models.CharField(max_length=200)
     data_adcionada = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         """Devolve uma representação em string do modelo."""
         return self.texto 
+    
+class Entrada(models.Model):
+    """Algo específico aprendido sobre um assunto."""
+    topico = models.ForeignKey(Topico, on_delete=models.CASCADE)
+    texto = models.TextField()
+    data_adcionada = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Ela nos permite definir um atributo especial que diz a Django para usar Entries quando precisar se referir a mais de uma entrada."""
+        verbose_name_plural = "Entradas"
+
+    def __str__(self):
+        """quais informações devem ser mostradas quando entradas individuais forem referenciadas."""
+        return self.texto[:50] + "..."
+    
 
