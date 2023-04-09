295a406bb99dae7180b61b0b2727596d0c226c73
Carlos Roberto <carlosrjhoe@gmail.com>
2023-04-09 08:00:40 -0300

    Estudando funções em python

---
+++ Curso_python_03_basico_ao_avançado_udemy/Python_Intermediário/Funções/funcao.py
@@ -0,0 +1,6 @@
+def dados(nome="Nome não informado", idade="Idade não informada"):
+    print(f"Dados:\nNome: {nome}\nIdade: {idade}")
+
+if __name__ == "__main__":
+    dados()
+    dados("Carlos", 37)
