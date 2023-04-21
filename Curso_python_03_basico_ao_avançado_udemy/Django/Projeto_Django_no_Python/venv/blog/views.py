90f807fb719ae387c740fa8382c6464163e72954
Carlos Roberto <carlosrjhoe@gmail.com>
2023-04-21 13:30:06 -0300

    Estudando Django: Configurando arquivos templates e projeto

--- Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_Django_no_Python/venv/blog/views.py
+++ Curso_python_03_basico_ao_avançado_udemy/Django/Projeto_Django_no_Python/venv/blog/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
-
-def sobre(request):
-    return render(request, 'blog/sobre.html')
