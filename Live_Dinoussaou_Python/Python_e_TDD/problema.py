9acfbb540b47dee9c50b1b4e54faa5644db58e6a
Carlos Roberto <carlosrjhoe@gmail.com>
2023-04-23 09:53:47 -0300

    Estudando TDD com Python

--- Live_Dinoussaou_Python/Python_e_TDD/problema.py
+++ Live_Dinoussaou_Python/Python_e_TDD/problema.py
@@ -4,7 +4,8 @@ Numeros = [3, 4, 5, 0, 1, -14, 20, 2, 7, 10, -2]
 from typing import List
 
 def problema(numeros: List[int]) -> int:
-    valores_filtrados = list(filter(lambda x: x > 0, sorted(numeros)))
+    valores_filtrados = [n for n in sorted(numeros) if n > 0]
+    # valores_filtrados = list(filter(lambda x: x > 0, sorted(numeros)))
 
     if not valores_filtrados:
         return 1
@@ -14,3 +15,5 @@ def problema(numeros: List[int]) -> int:
     for i, j in zip(valores_filtrados, r):
         if i != j:
             return j
+
+    return valores_filtrados[-1] + 1
--- Live_Dinoussaou_Python/Python_e_TDD/tests.py
+++ Live_Dinoussaou_Python/Python_e_TDD/tests.py
@@ -1,28 +1,32 @@
-"""
-Numeros = [0, 3, 10, -2, -14, 20]
-"""
-
 from unittest import TestCase
 from problema import problema
 
 class TestProblema(TestCase):
     """
-    [ ] A -> Existem n numeros positivos
-    [ ] B -> Não existem numeros positivos -> 1
-    [ ] C -> Lista vazia -> 1
-    [ ] D -> Não exitem numeros faltantes -> LAST + 1
+        [x] A -> Existem numeros positivos
+        [x] B -> Não existem numeros positivos -> 1
+        [x] C -> Lista vazia -> 1
+        [x] D -> Não exitem numeros faltantes -> LAST + 1
     """
   
     def test_problema_de_retornar_5(self):
         esperado = 6
         resultado = problema([ 3, 4, 5, 0, 1, -14, 20, 2, 7, 10, -2])
-
         self.assertEqual(esperado, resultado)
 
     def test_problema_de_retornar_1(self):
         esperado = 1
         resultado = problema([ -14, -20, -2, -7, -10, -2])
+        self.assertEqual(esperado, resultado)
+        
+    def test_problema_de_retornar_1_quando_nao_houver_elementos_na_lista(self):
+        esperado = 1
+        resultado = problema([])
+        self.assertEqual(esperado, resultado)
 
+    def test_problema_de_retornar_ultimo_mais_1_quando_nao_houver_numero_faltante(self):
+        esperado = 10
+        resultado = problema([1,2,3,4,5,6,7,8,9])
         self.assertEqual(esperado, resultado)
     
     
