c5692e70595ed20440412790143179bf945f3c55
Carlos Roberto <carlosrjhoe@gmail.com>
2023-05-04 19:30:59 -0300

    Estudando Django: Atualizando arquivos

--- Curso_python_03_basico_ao_avançado_udemy/Django/Primeiro_projeto/django/blog/views.py
+++ Curso_python_03_basico_ao_avançado_udemy/Django/Primeiro_projeto/django/blog/views.py
@@ -3,9 +3,7 @@ from blog.data import posts
 
 # Create your views here.
 def index(request):
-    
     context = {
         'posts': posts
     }
-    
     return render(request, 'blog/index.html', context)
