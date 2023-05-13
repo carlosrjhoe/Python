462342659ee259f3d3d96487e495b3f3d7eb8016
Carlos Roberto <carlosrjhoe@gmail.com>
2023-05-13 20:19:47 -0300

    Estudando Django: Configurando post único/Exibindo o erro404

---
+++ Curso_python_03_basico_ao_avançado_udemy/Python_Intermediário/Funções/escopo.py
@@ -0,0 +1,9 @@
+nome_2 = 'Mayara'
+def imprimirNome(nome_1):
+   nome_2 = 'Neto'
+   print(f'{nome_1}')
+   print(f'{nome_2}')
+
+if __name__ == '__main__':
+    print(f'{nome_2}')
+    imprimirNome('Carlos')
