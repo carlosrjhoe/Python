7ce9eb425b0c81f1fdd1dcc540d7243123061ded
Carlos Roberto <carlosrjhoe@gmail.com>
2023-05-13 21:13:06 -0300

    Estudando Python: Funções

--- Curso_python_03_basico_ao_avançado_udemy/Python_Intermediário/Funções/escopo.py
+++ Curso_python_03_basico_ao_avançado_udemy/Python_Intermediário/Funções/escopo.py
@@ -1,8 +1,11 @@
-nome_2 = 'Mayara'
+nome_2 = 'Mayara' # Variável no escopo global
+
+
 def imprimirNome(nome_1):
-   nome_2 = 'Neto'
-   print(f'{nome_1}')
-   print(f'{nome_2}')
+    nome_2 = 'Neto' # Variável no escopo local
+    print(f'{nome_1}')
+    print(f'{nome_2}')
+
 
 if __name__ == '__main__':
     print(f'{nome_2}')
---
+++ Curso_python_03_basico_ao_avançado_udemy/Python_Intermediário/Funções/return.py
@@ -0,0 +1,23 @@
+def soma(num_1, num_2):
+    resultado = num_1 + num_2
+    return resultado
+
+def subtracao(num_1, num_2):
+    resultado = num_1 - num_2
+    return resultado
+
+def multiplicacao(num_1, num_2):
+    resultado = num_1 * num_2
+    return resultado
+
+def divisao(num_1, num_2):
+    resultado = num_1 / num_2
+    return int(resultado)
+
+
+
+if __name__ == '__main__':
+    print(soma(4,2))
+    print(subtracao(4,2))
+    print(multiplicacao(4,2))
+    print(divisao(4,2))
