fd8d0d66d8032ff93907c9aa032075e3c2aa9c17
Carlos Roberto <carlosrjhoe@gmail.com>
2023-05-17 19:49:49 -0300

    Estudando Python: validação de senha(Adicionando comentários)

--- Livro_Python_na_Prática_2/validacao_senha.py
+++ Livro_Python_na_Prática_2/validacao_senha.py
@@ -5,22 +5,27 @@ import re
 
 def cadastrar_senha():
     senha = input('Insira sua senha: ')
-    x = True
+    x = True # variável de controle
 
     while x:
         if len(senha) < 6 or len(senha) > 16:
+            # Verifica se o tamanho de senha é menor que 6 ou maior que 16
             print(f'A senha deve conter entre 6 e 16 caracteres.')
             break
         elif not re.search('[a-z]', senha):
+            # Verifica se o retorno do método com justaposição, neste caso, caracteres minúsculos de A até Z.
             print('A senha deve ter ao menos uma letra minúcula.')
             break
         elif not re.search('[0-9]', senha):
+            # Verifica se o retorno do método com justaposição, neste caso, não existem números de 0 até 9.
             print('A senha deve conter ao menos um número.')
             break
         elif not re.search('[@#$%&*!]', senha):
+            # Verifica se o retorno do método com justaposição, neste caso, não existem caracteres especiais.
             print('A senha deve conter ao menos um caractere especial')
             break
         elif re.search('\s', senha):
+            # Verifica no retorno do método com justaposição, neste caso, se contem espaças em branco.
             break
         else:
             print('Senha cadastrada com sucesso!!!')
