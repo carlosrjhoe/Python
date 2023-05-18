bffcb0ac9671c64375497bcbb725b86de690c9c9
Carlos Roberto <carlosrjhoe@gmail.com>
2023-05-17 21:08:28 -0300

    Estudando Python: Organizando lista

---
+++ Livro_Python_na_Prática_2/lista_dict.py
@@ -0,0 +1,16 @@
+"""Supondo que tenhamos uma lista composta por elementos em forma de dicionário, elementos estes aleatórios descrevendo em suas chaves e valores alguns carros com seus respectivos anos de fabricação, reorganize os elementos dessa lista de acordo com
+"""
+
+def modelos_carros(carros):
+    return carros
+
+if __name__ == '__main__':
+    carros = [
+        {'carro': 'celta', 'ano': 2005},
+        {'carro': 'gol', 'ano': 2000},
+        {'carro': 'palio', 'ano': 2019},
+        {'carro': 'corsa', 'ano': 2011},
+        {'carro': 'fiesta', 'ano': 2009},
+    ]
+
+    print(f'{modelos_carros(carros)}')
