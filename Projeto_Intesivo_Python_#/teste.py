50f94c2ecbeb2d05508a2ac7ea5373374ad54ad5
Carlos Roberto <carlosrjhoe@gmail.com>
2023-05-20 19:44:49 -0300

    Estudando Python

---
+++ Projeto_Intesivo_Python_#/teste.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+
+pyautogui.PAUSE = 0.5
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+pyautogui.write('https://www.udemy.com/')
+pyautogui.press('enter')
