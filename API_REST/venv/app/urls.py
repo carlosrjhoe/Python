b692a3cc7ecd8e74eca287696facca63af33acf5
Carlos Roberto <carlosrjhoe@gmail.com>
2023-06-03 23:14:38 -0300

    APIRest: Criando so metodos PUT e DELETE

--- API_REST/venv/app/urls.py
+++ API_REST/venv/app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from app.views import todo_list
+from app.views import todo_list, todo_detail_change_and_delete
 
 app_name = 'app'
 
 urlpatterns = [
     path('', todo_list),
+    path('<int:pk>/', todo_detail_change_and_delete),
 ]
--- API_REST/venv/app/views.py
+++ API_REST/venv/app/views.py
@@ -11,8 +11,27 @@ def todo_list(request):
         serializer = TodoSerializers(todo, many=True) # Class de serializer
         return Response(serializer.data) # Response
     elif request.method == 'POST': # Pegar adicionar novos objetos
-        serializer = TodoSerializers(data=request.data)
+        serializer = TodoSerializers(data=request.data) # Trasformar objeto em ebjeto JSON
         if serializer.is_valid(): # Verificar objeto está correto
             serializer.save() # Salvar objeto
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE']) # Função decorator com argumentos (GET e PUT e DELETE)
+def todo_detail_change_and_delete(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = TodoSerializers(todo)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = TodoSerializers(todo, data=request.data) 
+        if serializer.is_valid(): 
+            serializer.save() 
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
