502a132ede4174d4af9c8f7466552e6eedd582ee
Carlos Roberto <carlosrjhoe@gmail.com>
2023-06-04 00:42:20 -0300

    APIRest: Finalizada

--- API_REST/venv/app/views.py
+++ API_REST/venv/app/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
+# from rest_framework import generics
 
 class TodoListAndCreate(APIView):
     def get(self, request):
