d9365e1b89855007c650bea56426534bfb8ff3e0
Carlos Roberto <carlosrjhoe@gmail.com>
2023-06-04 10:34:05 -0300

    Estudando Funções: Closure

---
+++ Curso_python_03_basico_ao_avançado_udemy/Python_Intermediário/Funções/closure.py
@@ -0,0 +1,22 @@
+"""
+Função que retorna outra função
+"""
+
+def saudacao(saudacao):
+    def saudar(nome):
+        return f'{saudacao}, {nome}'
+    return saudar
+
+saudacao_bom_dia = saudacao('Bom dia')
+saudacao_boa_noite = saudacao('Boa Noite')
+
+for nome in ['carlos', 'mayara']:
+    print(f'{saudacao_bom_dia(nome.title())}')
+
+for nome in ['neto', 'luna']:
+    print(f'{saudacao_boa_noite(nome.title())}')
+
+# >>> Bom dia, Carlos
+# >>> Bom dia, Mayara
+# >>> Boa Noite, Neto
+# >>> Boa Noite, Luna
