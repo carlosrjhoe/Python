f4da93d3e5b94eda9fe64c9c4b1c808ed67ecc06
Carlos Roberto <carlosrjhoe@gmail.com>
2023-06-22 05:20:56 -0300

    Set

---
+++ Curso_python_03_basico_ao_avançado_udemy/Python_Intermediário/Dicionarios/set.py
@@ -0,0 +1,10 @@
+# Cria um set vázio
+set_1 = set('')
+print(set_1, type(set_1))
+
+# Cria um set com valor
+set_2 = {'carlos', 1, 2, 2, 2, 3, 3, 4, 4, 5}
+print(set_2, type(set_2))
+
+for itens in set_2:
+    print(itens)     
