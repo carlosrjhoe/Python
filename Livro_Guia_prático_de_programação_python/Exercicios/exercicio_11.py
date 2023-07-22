041cfee593dd486d8a5a6261ff40b3d0e8d12b7b
Carlos Roberto <carlosrjhoe@gmail.com>
2023-07-22 17:59:20 -0300

    Livro guia prático de programação com python: exercicios

---
+++ Livro_Guia_prático_de_programação_python/Exercicios/exercicio_11.py
@@ -0,0 +1,9 @@
+contatos = {
+    'carlos': '99988-7766',
+    'mayara': '98877-6655',
+    'neto': '97766-5544',
+    'luna': '95544-3322',
+}
+
+for nome, telefone in contatos.items():
+    print(f'O telefone de {nome.title()} é {telefone}')
---
+++ Livro_Guia_prático_de_programação_python/Exercicios/exercicio_12.py
@@ -0,0 +1,7 @@
+dados_pessoa = {}
+
+dados_pessoa['nome'] = input('Digite seu nome: ')
+dados_pessoa['telefone'] = input('Digite seu telefone: ')
+dados_pessoa['email'] = input('Digite seu E-mail: ')
+
+print(dados_pessoa)
