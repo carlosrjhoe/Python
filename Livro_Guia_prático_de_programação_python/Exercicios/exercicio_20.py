b95677123d16ce2c1687c6c081d09ae23e573eb9
Carlos Roberto <carlosrjhoe@gmail.com>
2023-07-23 22:20:57 -0300

    Livro guia prático de programação com python: exercicios

--- Livro_Guia_prático_de_programação_python/Exercicios/exercicio_20.py
+++ Livro_Guia_prático_de_programação_python/Exercicios/exercicio_20.py
@@ -1,31 +1,54 @@
-tipo_combustivel = input('''TIPO DO COMBUSTÍVEL:\n[ A ] - Álcool\n[ G ] - Gasolina\nQual a opção? ''').upper()
-qnt_litros = float(input('Quantidade de litros? '))
+def verificar_tipo_combustivel(tipo_combustivel):
+    if tipo_combustivel == 'A':
+        nome_tipo_combustivel = 'Álcool'
+    else:
+        nome_tipo_combustivel = 'Gasolina'
+    return nome_tipo_combustivel
 
-nome_tipo_combustivel = ''
-valor = 0
+def verificar_valor_combustivel(tipo_combustivel, qnt_litros):
+    if tipo_combustivel == 'A':
+        valor = qnt_litros * 1.90
+    else:
+        valor = qnt_litros * 2.50
+    return valor
+
 valor_desconto = 0
 valor_com_desconto = 0
 
-if tipo_combustivel == 'A':
-    nome_tipo_combustivel = 'Álcool'
-    valor = qnt_litros * 4.19
+def verificar_desconto(verificar_valor_combustivel, tipo_combustivel, qnt_litros, valor_desconto):
     if qnt_litros > 20:
-        valor_desconto = valor * 0.05
-        valor_com_desconto = valor - valor_desconto
+        valor_desconto = verificar_valor_combustivel(tipo_combustivel, qnt_litros) * 0.05
     else:
-        valor_desconto = valor * 0.03
-        valor_com_desconto = valor - valor_desconto
-else:
-    nome_tipo_combustivel = 'Gasolina'
-    valor = qnt_litros * 5.59
-    if qnt_litros > 20:
-        valor_desconto = valor * 0.05
-        valor_com_desconto = valor - valor_desconto
-    else:
-        valor_desconto = valor * 0.03
-        valor_com_desconto = valor - valor_desconto
+        valor_desconto = verificar_valor_combustivel(tipo_combustivel, qnt_litros) * 0.03
+    return valor_desconto
 
-print('\n\tResumo da compra\n\t')
-print(f'Tipo de combustivel: {nome_tipo_combustivel}')
-print(f'Quantidade de combustivel(L): {qnt_litros:1.2f}L')
-print(f'Valor total: R${valor:1.2f}')
+def resumo():
+    print('\n\tResumo da compra\n\t')
+    print(f'Tipo de combustivel: \t\t{verificar_tipo_combustivel(tipo_combustivel)}')
+    print(f'Quantidade de combustivel: \t{qnt_litros}L')
+    print(f'Valor total: \t\t\tR${verificar_valor_combustivel(tipo_combustivel, qnt_litros):1.2f}')
+    print(f'Valor do desconto: \t\tR${verificar_desconto(verificar_valor_combustivel, tipo_combustivel,qnt_litros, valor_desconto):1.2f}')
+    print(f'Valor com desconto: \t\tR${verificar_valor_combustivel(tipo_combustivel, qnt_litros) - verificar_desconto(verificar_valor_combustivel, tipo_combustivel,qnt_litros, valor_desconto):1.2f}')
+
+if __name__ == '__main__':
+    
+    tipo_combustivel = input('TIPO DO COMBUSTÍVEL:\n[ A ] - Álcool\n[ G ] - Gasolina\nQual a opção? ').upper()
+    qnt_litros = float(input('Quantidade de litros? '))
+
+    verificar_tipo_combustivel(tipo_combustivel)
+    verificar_valor_combustivel(tipo_combustivel, qnt_litros)
+    resumo()
+
+    # TIPO DO COMBUSTÍVEL:
+    # [ A ] - Álcool
+    # [ G ] - Gasolina
+    # Qual a opção? a
+    # Quantidade de litros? 20
+    # 
+            # Resumo da compra
+    # 
+    # Tipo de combustivel:            Álcool
+    # Quantidade de combustivel:      20.0L
+    # Valor total:                    R$38.00
+    # Valor do desconto:              R$1.90
+    # Valor com desconto:             R$36.10
