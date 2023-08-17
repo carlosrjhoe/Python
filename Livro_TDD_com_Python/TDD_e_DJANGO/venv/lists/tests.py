3fe698653e9442c7f993cb29113529be335e3190
Carlos Roberto <carlosrjhoe@gmail.com>
2023-08-16 21:21:59 -0300

    Livro - TDD com python: Criando um test para uma view básica do App lists

--- Livro_TDD_com_Python/TDD_e_DJANGO/venv/lists/tests.py
+++ Livro_TDD_com_Python/TDD_e_DJANGO/venv/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from .views import index
@@ -8,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_lists_2(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
+
+    def test_index_return_corret_html(self):
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
