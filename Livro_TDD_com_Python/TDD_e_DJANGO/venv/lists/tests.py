14e3b9c6bc24b75c5069ebd95ce7a81cb1bd7289
Carlos Roberto <carlosrjhoe@gmail.com>
2023-08-23 20:33:00 -0300

    Livro - TDD com python: Refatoração lists/tests.py

--- Livro_TDD_com_Python/TDD_e_DJANGO/venv/lists/tests.py
+++ Livro_TDD_com_Python/TDD_e_DJANGO/venv/lists/tests.py
@@ -5,18 +5,8 @@ from .views import index
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_url_lists_2(self):
-        found = resolve('/')
-        self.assertEqual(found.func, index)
-
     def test_index_return_corret_html(self):
         # Chamar a view function de forma direta
         response = self.client.get('/')
-        # Extrair bytes puro da resposta e converter o html em strigs 
-        html = response.content.decode('UTF-8')
-        # Verificar se a pagina é uma página padrão HTML
-        self.assertTrue('<html>', html)
-        # Verificar o titilo da pagina renderizada
-        self.assertIn('<title>To-Do lists</title>', html)
         # Verificar qual template foi redenrizado
         self.assertTemplateUsed(response, 'lists/index.html')
