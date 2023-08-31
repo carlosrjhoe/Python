bcbe614acfcb90b879396c70240a78414074c29b
Carlos Roberto <carlosrjhoe@gmail.com>
2023-08-31 19:51:58 -0300

    Python - POO: Primeiros passos projeto POO

--- Python_entendendo_Orientação_a_Objeto/teste.py
+++ Python_entendendo_Orientação_a_Objeto/teste.py
@@ -1,4 +1,21 @@
-def create_account(number,holder ,balance, limit):
-    account_01 = {'number': number,'holder': holder,'balance': balance, 'limit': limit}
-    return account_01
+def cria_conta(numero, titular, saldo, limite):
+    conta = {'numero': numero, 'titular': titular, 'saldo': saldo, 'limite': limite}
+    return conta
+
+def depositar(conta, valor):
+    conta['saldo'] += valor
+
+def sacar(conta, valor):
+    conta['saldo'] -= valor
+
+def extrato(conta):
+    print(f'Saldo: {conta["saldo"]}')
     
+
+if __name__ == '__main__':
+    conta_01 = cria_conta(123, 'Carlos', 1000, 1100)
+    extrato(conta_01)
+    depositar(conta_01, 1000)
+    extrato(conta_01)
+    sacar(conta_01, 500)
+    extrato(conta_01)
