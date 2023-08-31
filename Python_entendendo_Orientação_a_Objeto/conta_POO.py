c8618159620b3dfbee22903b4047f5eddd948768
Carlos Roberto <carlosrjhoe@gmail.com>
2023-08-31 20:51:17 -0300

    Python - POO: Classes e Objetos

---
+++ Python_entendendo_Orientação_a_Objeto/conta_POO.py
@@ -0,0 +1,9 @@
+class Conta():
+    # Defina a classe
+    def __init__(self, numero, titular, saldo, limite):
+        # Defina a função construtora da classe com atributos
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
