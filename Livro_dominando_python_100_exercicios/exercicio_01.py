9ef303b4dbf5b6c6f8b3f18e0671cb196a1781dc
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-02 12:49:52 -0300

    Livro Dominando python 100 exercicio

--- Livro_dominando_python_100_exercicios/exercicio_01.py
+++ Livro_dominando_python_100_exercicios/exercicio_01.py
@@ -1,20 +1,25 @@
-def soma(num_1, num_2):
-    return num_1 + num_2
+class Calculadora:
+    def __init__(self, fator_1, fator_2):
+        self.fator_1 = fator_1
+        self.fator_2 = fator_2
+        
+    def soma(self):
+        return  self.fator_1 + self.fator_2 
 
-def subtracao(num_1, num_2):
-    return num_1 - num_2
+    def subtracao(self):
+        return self.fator_1 - self.fator_2
 
-def multiplicacao(num_1, num_2):
-    return num_1 * num_2
+    def multiplicacao(self):
+        return self.fator_1 * self.fator_2
 
-def divisao(num_1, num_2):
-    return num_1 / num_2
-    
-def resulmo():
-    print(f'A soma dos números {num_1} e {num_2} = {soma(num_1, num_2)}')
-    print(f'A subitração dos números {num_1} e {num_2} = {subtracao(num_1, num_2)}')
-    print(f'A multiplicação dos números {num_1} e {num_2} = {multiplicacao(num_1, num_2)}')
-    print(f'A divisão dos números {num_1} e {num_2} = {divisao(num_1, num_2)}')
+    def divisao(self):
+        return self.fator_1 / self.fator_2
+        
+    def resulmo(self):
+        print(f'A soma dos números {self.fator_1} e {self.fator_2}')
+        print(f'A subitração dos números {self.fator_1} e {self.fator_2}')
+        print(f'A multiplicação dos números {self.fator_1} e {self.fator_2}')
+        print(f'A divisão dos números {self.fator_1} e {self.fator_2}')
 
     
 if __name__ == '__main__':
