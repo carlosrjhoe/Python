c691b0b03823abaa8315c54fea33d183458fb849
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-02 04:38:04 -0300

    Python - POO: Atributos privádos

--- Python_entendendo_Orientação_a_Objeto/conta_POO.py
+++ Python_entendendo_Orientação_a_Objeto/conta_POO.py
@@ -2,19 +2,19 @@ class Conta():
     # Define a classe
     def __init__(self, numero, titular, saldo, limite):
         # Define a função construtora da classe com atributos
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def depositar(self, valor):
         # Metodo depositar
-        self.saldo += valor
+        self.__saldo += valor
     
     def sacar(self, valor):
         # Metodo sacar
-        self.saldo -= valor
+        self.__saldo -= valor
     
     def extrato(self):
         # Metodo extrato
-        return f'Titular: {self.titular} - Saldo: {self.saldo}'
+        return f'Titular: {self.__titular.title()} - Saldo: {self.__saldo}'
