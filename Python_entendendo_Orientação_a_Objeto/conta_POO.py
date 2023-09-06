33fe941964a59f73d49593d7ac0639a20d0928fd
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-05 22:08:47 -0300

    Python - POO: Métodos privados, métodos de Classe e métodos estáticos

--- Python_entendendo_Orientação_a_Objeto/conta_POO.py
+++ Python_entendendo_Orientação_a_Objeto/conta_POO.py
@@ -7,6 +7,17 @@ class Conta():
         self.__saldo = saldo
         self.__limite = limite
 
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
+
+    def extrato(self):
+        return f'Nome: {self.__titular.title()} - Saldo: R${self.__saldo}'
+
     @property
     def numero(self):
         return self.__numero
@@ -27,17 +38,29 @@ class Conta():
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    def __pode_sacar(self, valor):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor <= valor_disponivel_a_sacar
     
     def sacar(self, valor):
         # Metodo sacar
-        self.__saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print(f'O valor R${valor:.2f} passou o limite.')
     
     def depositar(self, valor):
         # Metodo depositar
         self.__saldo += valor
+        
+    def __pode_transferir(self, valor):
+        valor_disponivel_a_transferir = self.__saldo + self.__limite
+        return valor <= valor_disponivel_a_transferir
 
     def transferir(self, valor, destino):
-        self.sacar(valor)
-        destino.depositar(valor)
+        if (self.__pode_transferir(valor)):
+            self.sacar(valor)
+            destino.depositar(valor)
         msg = f'Transferindo R${valor:.2f} para {destino.__titular.title()}'
         return msg
