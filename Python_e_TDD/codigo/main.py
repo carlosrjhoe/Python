47c065e3388d329cc19719e47e5040a51d9399da
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-08 05:03:35 -0300

    Python e TDD: Atualizando Testes automatizados

--- Python_e_TDD/codigo/main.py
+++ Python_e_TDD/codigo/main.py
@@ -1,7 +1,12 @@
 from bytebank import Funcionario
 
-carlos = Funcionario('carlos', '03/11/1985', 3139)
+def test_idade():
+    teste_01 = Funcionario('carlos', '03/11/1985', 3139)
+    teste_02 = Funcionario('mayara', '04/01/1985', 3139)
+    teste_03 = Funcionario('neto', '31/10/2015', 3139)
 
-print(carlos.idade())
-print(carlos.salario)
-print(carlos.calcular_bonus())
+    print(f'Teste = {teste_01.idade()}')
+    print(f'Teste = {teste_02.idade()}')
+    print(f'Teste = {teste_03.idade()}')
+
+test_idade()
