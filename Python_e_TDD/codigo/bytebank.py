86927e83de7895bf20465ff7b414580f06244e32
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-08 19:22:32 -0300

    Python e TDD: Pytest com Test-Driven-Development

--- Python_e_TDD/codigo/bytebank.py
+++ Python_e_TDD/codigo/bytebank.py
@@ -14,6 +14,11 @@ class Funcionario:
     def salario(self):
         return self._salario
 
+    def sobre_nome(self):
+        nome_completo = self._nome.strip()
+        sobre_nome = nome_completo.split(' ')
+        return sobre_nome[-1]
+
     def idade(self):
         data_formatada = self._data_nascimento.split('/')
         ano = data_formatada[-1]
@@ -28,3 +33,10 @@ class Funcionario:
 
     def __str__(self):
         return f'Funcionario({self._nome}, {self._data_nascimento}, {self._salario})'
+
+if __name__ == '__main__':
+    carlos = Funcionario('carlos', '03/11/1985', 3139)
+    print(type(carlos.nome))
+    print(type(carlos._data_nascimento))
+    print(type(carlos._salario))
+    print(type(carlos.idade()))
--- Python_e_TDD/tests/test_bytebank.py
+++ Python_e_TDD/tests/test_bytebank.py
@@ -1,10 +1,29 @@
 from codigo.bytebank import Funcionario
 
 class TestClass:
+    
     def test_quando_idade_recebe_03_11_1985_deve_receber_38(self):
-        entrada = '03/11/1985/'
+        """
+        DADO a data de nascimento
+        QUANDO o metodo idade é chamada
+        ENTÃO deve retornar a idade
+        """
+        entrada = '03/11/1985'
         esperado = 38
         funcionario_test = Funcionario('carlos', entrada, 3139)
         resultado = funcionario_test.idade()
+
+        assert resultado == esperado
+
+    def test_quando_sobrenome_recebe_carlos_conceicao_deve_retornar_conceicao(self):
+        """
+        DADO um nome com sobrenome
+        QUANDO o metodo sobre_nome é chamado
+        ENTÃO deve retornar o sobrenome
+        """
+        entrada = 'carlos conceição'
+        esperado = 'conceição'
+        funcionario_test = Funcionario('carlos conceição', entrada, 3139)
+        resultado = funcionario_test.sobre_nome()
 
         assert resultado == esperado
