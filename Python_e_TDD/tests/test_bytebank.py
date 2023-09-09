40ed2e820f71a73fe57ec6b4028b3745b9fcf136
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-09 12:38:49 -0300

    Python e TDD: GIVEN, WHEN and THEN

--- Python_e_TDD/tests/test_bytebank.py
+++ Python_e_TDD/tests/test_bytebank.py
@@ -4,9 +4,9 @@ class TestClass:
     
     def test_quando_data_de_nascimento_recebe_03_11_1985_deve_retornar_38(self):
         """
-        DADO a data de nascimento
-        QUANDO o metodo idade é chamada
-        ENTÃO deve retornar a idade
+        GIVEN: a data de nascimento
+        WHEN: o metodo idade é chamada
+        THEN: deve retornar a idade
         """
         esperado = 38
         funcionario_test = Funcionario('carlos', '03/11/1985', 3139)
@@ -16,9 +16,9 @@ class TestClass:
 
     def test_quando_sobrenome_recebe_carlos_conceicao_deve_retornar_conceicao(self):
         """
-        DADO um nome com sobrenome
-        QUANDO o metodo sobre_nome é chamado
-        ENTÃO deve retornar o sobrenome
+        GIVEN: um nome com sobrenome
+        WHEN: o metodo sobre_nome é chamado
+        THEN: deve retornar o sobrenome
         """
         esperado = 'conceição'
         funcionario_test = Funcionario('carlos conceição', '03/11/1985', 3139)
@@ -28,9 +28,9 @@ class TestClass:
 
     def test_quando_desconto_recebe_salario_deve_ser_descontado_10_por_cento(self):
         """
-        DADO um valor do salario
-        QUANDO o metodo desconto é chamado
-        ENTÃO deve retornar o salario com desconto de 10 por cento
+        GIVEN: um valor do salario
+        WHEN: o metodo desconto é chamado
+        THEN: deve retornar o salario com desconto de 10 por cento
         """
         esperado = 90000
         funcionario_test = Funcionario('carlos conceição', '03/11/1985', 100000)
