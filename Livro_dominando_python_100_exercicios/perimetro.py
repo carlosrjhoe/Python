5b36baa7275d1bc1b36321f8bdefd52404baf34c
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-10 08:38:09 -0300

    Livro Dominando python 100 exercicios: Melhorando exercicio 04

---
+++ Livro_dominando_python_100_exercicios/perimetro.py
@@ -0,0 +1,13 @@
+from math import pi
+
+def calculates_perimeter(radius):
+    perimeter = 2 * pi * radius
+    return f'{perimeter:1.3f}'
+
+def displays_result():
+    print(f'The perimeter of the circle is: {calculates_perimeter(radius)}')
+    
+if __name__ == "__main__":
+    radius = float(input('Type the radius of the circle: '))
+    calculates_perimeter(radius)
+    displays_result()
