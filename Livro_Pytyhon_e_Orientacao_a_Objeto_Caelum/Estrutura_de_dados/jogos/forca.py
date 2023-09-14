689231f6d308812d3aff033e4a7161f9c2360522
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-13 21:50:56 -0300

    Livro_Pytyhon_e_Orientacao_a_Objeto_Caelum: Criando cabeçalho do jogo da forca

---
+++ Livro_Pytyhon_e_Orientacao_a_Objeto_Caelum/Estrutura_de_dados/jogos/forca.py
@@ -0,0 +1,8 @@
+def cabecalho():
+    TEXTO = 'BEM VIDO AO JOGO DA FORCA'
+    print(f'{"#"*len(TEXTO)}')
+    print(f'{TEXTO.center(len(TEXTO))}')
+    print(f'{"#"*len(TEXTO)}')
+
+if __name__ == "__main__":
+    cabecalho()
