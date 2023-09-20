aacdece8d0b6e047ebed363bfffbeb9b40026481
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-19 22:02:04 -0300

    Projeto TDD Django: URL do servidor e teste de falha

--- Python_e_TDD/TDD_Django/setup/test.py
+++ Python_e_TDD/TDD_Django/setup/test.py
@@ -4,8 +4,16 @@ from selenium.webdriver import Chrome
 class AnimaisTestCase(LiveServerTestCase):
 
     def setUp(self):
-        '''Configurar ambiente de testes'''
-        self.driver = Chrome('chromedriver.exe')
+        '''Abrir Webdriver'''
+        self.driver = Chrome()
 
     def tearDown(self) -> None:
+        '''Fechar Webdriver'''
         self.driver.quit()
+
+    def test_para_verificar_janela_webdriver_esta_ok(self):
+        self.driver.get(self.live_server_url)
+
+    def test_deu_errado(self):
+        '''Teste de exemplo de erro'''
+        self.fail('Teste falhou...')
