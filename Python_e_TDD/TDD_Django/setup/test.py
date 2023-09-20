784c10094a950c36d099804f899b44e824d24387
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-20 20:35:55 -0300

    Projeto TDD Django: Criando App animais

--- Python_e_TDD/TDD_Django/setup/test.py
+++ Python_e_TDD/TDD_Django/setup/test.py
@@ -1,5 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
 
 class AnimaisTestCase(LiveServerTestCase):
 
@@ -11,13 +13,19 @@ class AnimaisTestCase(LiveServerTestCase):
         '''Fechar Webdriver'''
         self.driver.quit()
 
-    def test_para_verificar_janela_webdriver_esta_ok(self):
-        self.driver.get(self.live_server_url)
-
-    def test_deu_errado(self):
-        '''Teste de exemplo de erro'''
-        self.fail('Teste falhou...')
-
     def test_buscando_um_novo_animal(self):
         '''Teste buscar animal na pesquisa'''
+        # Ele encontra o Busca Animal e decide usar o site
+        home_page = self.driver.get(self.live_server_url)
         
+        # porque ele vê no menu do site escrito Busca Animal.
+        nav_element = self.driver.find_element(By.CLASS_NAME, '.navbar')
+        self.assertEqual('Busca Animal', nav_element.text)
+        
+        # Ele vê um campo para pesquisar animais pelo nome. 
+        
+        # Ele pesquisa por Leão e clica no botão pesquisar.
+
+        # O site exibe 4 caracteristicas do animal pesquisado.
+    
+        # Ele desiste de adotar um leão.
