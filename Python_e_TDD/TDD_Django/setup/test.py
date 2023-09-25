9a95a443666306ddaeb6a3afa232939f65d3c349
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-25 19:54:25 -0300

    Projeto TDD Django: Criando test funcional com AssertGreater

--- Python_e_TDD/TDD_Django/setup/test.py
+++ Python_e_TDD/TDD_Django/setup/test.py
@@ -14,20 +14,23 @@ class AnimaisTestCase(LiveServerTestCase):
         self.driver.quit()
 
     def test_buscando_um_novo_animal(self):
-        '''Teste buscar animal na pesquisa'''
-        # Ele encontra o Busca Animal e decide usar o site
+        '''Teste buscar animal na pesquisa, ele encontra o Busca Animal e decide usar o site'''
         home_page = self.driver.get(self.live_server_url)
         
-        # porque ele vê no menu do site escrito Busca Animal.
+        '''porque ele vê no menu do site escrito Busca Animal.'''
         nav_element = self.driver.find_element(By.CLASS_NAME, 'navbar')
         self.assertEqual('Busca Animal', nav_element.text)
         
-        # Ele vê um campo para pesquisar animais pelo nome. 
+        '''Ele vê um campo para pesquisar animais pelo nome.''' 
         buscar_animal_input = self.driver.find_element(By.CSS_SELECTOR, 'input#buscar_animal')
         self.assertEqual(buscar_animal_input.get_attribute('placeholder'), 'Exemplo: Leão')
         
-        # Ele pesquisa por Leão e clica no botão pesquisar.
-
-        # O site exibe 4 caracteristicas do animal pesquisado.
+        '''Ele pesquisa por Leão e clica no botão pesquisar.'''
+        buscar_animal_input.send_keys('Leão')
+        self.driver.find_element(By.CSS_SELECTOR, 'form button').click()
+        
+        '''O site exibe 4 caracteristicas do animal pesquisado.'''
+        caracteristicas = self.driver.find_elements(By.CSS_SELECTOR, '.result-description')
+        self.assertGreater(len(caracteristicas), 3)
     
-        # Ele desiste de adotar um leão.
+        '''Ele desiste de adotar um leão.'''
