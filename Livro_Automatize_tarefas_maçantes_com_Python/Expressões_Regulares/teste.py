21c4ddee586f74a5071f7e5b44b33b916d839f3b
Carlos Roberto <carlosrjhoe@gmail.com>
2023-11-07 05:43:20 -0300

    Livro: Python e Orientação a Objetos (Caelum)

--- Livro_Automatize_tarefas_maçantes_com_Python/Expressões_Regulares/teste.py
+++ Livro_Automatize_tarefas_maçantes_com_Python/Expressões_Regulares/teste.py
@@ -1,25 +1,22 @@
-def textoPositivo(texto):
-    print(f'{texto} é um número válido!')
-    
 def eNumeroDeTelefone(texto):
     """Verificar se a string em texto é um número de telefone válido"""
-    if len(texto) != 14:
+    if len(texto) != 13:
         return False
     for i in range(0, 3):
         if not texto[i].isdecimal():
             return False
-    if texto[3] != '-':
+    if texto[2] != '-':
         return False
-    for i in range(4, 9):
+    for i in range(3, 7):
         if not texto[i].isdecimal():
             return False
-    if texto[9] != '-':
+    if texto[8] != '-':
         return False
-    for i in range(10, 14):
+    for i in range(9, 12):
         if not texto[i].isdecimal():
             return False
-    textoPositivo(texto)
+    return True
 
 if __name__ == '__main__':
-    textoPositivo('081-99541-9951')
-    textoPositivo('081-98794-9484')
+    print('81-99541-9951 é um número válido?')
+    print(eNumeroDeTelefone('81-99541-9951'))
