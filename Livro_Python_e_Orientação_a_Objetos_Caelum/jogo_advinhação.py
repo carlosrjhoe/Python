aa7c0e08076f53298984f7313601074d008c2dd0
Carlos Roberto <carlosrjhoe@gmail.com>
2023-11-09 05:11:04 -0300

    Livro Python e Orientação a Objetos (Caelum): Adicionando logica de tentativas e numero de rodadas

--- Livro_Python_e_Orientação_a_Objetos_Caelum/jogo_advinhação.py
+++ Livro_Python_e_Orientação_a_Objetos_Caelum/jogo_advinhação.py
@@ -5,14 +5,30 @@ def cabecalho():
     print(50*'*')
 
 def advinhacao():
+    tentativas = 2
+    rodada = 1
     numero_secreto = 42
-
-    chute = int(input('Digite o seu número: '))
-    print(f'Você digitou: {chute}')
-    if chute != numero_secreto:
-        print('Você errou!')
-    else:
-        print('Acertou!')
+    
+    while tentativas > 0:
+        print(f'{rodada}° rodada, você tem {tentativas} tentativas')
+        chute = int(input('Digite o seu número: '))
+        print(f'Você digitou: {chute}')
+        
+        acertou = chute == numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
+        
+        if acertou: 
+            print('Acertou!')
+            break
+        elif maior: 
+            print('Você errou, o chute foi maior que o número secreto.')
+            tentativas -= 1
+        elif menor: 
+            print('Você errou, o chute foi menor que o número secreto.')
+            tentativas -= 1
+            rodada += 1
+    print('Fim de jogo!')
     
 def main():
     cabecalho()
