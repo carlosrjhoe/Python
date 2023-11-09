32756c47b4bbdf78d3ea9e1cb40594014ecb17ff
Carlos Roberto <carlosrjhoe@gmail.com>
2023-11-09 04:54:30 -0300

    Livro Python e Orientação a Objetos (Caelum): Adicionando adivinhação

--- Livro_Python_e_Orientação_a_Objetos_Caelum/jogo_advinhação.py
+++ Livro_Python_e_Orientação_a_Objetos_Caelum/jogo_advinhação.py
@@ -4,5 +4,19 @@ def cabecalho():
     print(f'{texto:*^50}')
     print(50*'*')
 
-if __name__ == "__main__":
+def advinhacao():
+    numero_secreto = 42
+
+    chute = int(input('Digite o seu número: '))
+    print(f'Você digitou: {chute}')
+    if chute != numero_secreto:
+        print('Você errou!')
+    else:
+        print('Acertou!')
+    
+def main():
     cabecalho()
+    advinhacao()
+    
+if __name__ == "__main__":
+    main()
