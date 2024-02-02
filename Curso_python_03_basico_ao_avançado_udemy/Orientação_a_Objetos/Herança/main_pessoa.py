a12e20c8fce28dba427420b7c253eb7db0accc5e
Carlos Roberto <carlosrjhoe@gmail.com>
2024-02-02 20:49:11 -0300

    Voltando aos estudos do básico

---
+++ Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/Herança/main_pessoa.py
@@ -0,0 +1,10 @@
+from pessoa import Pessoa, Aluno, Cliente
+
+carlos = Pessoa('carlos', 'roberto')
+carlos.info()
+
+mayara = Cliente('mayara', 'ramos')
+mayara.info()
+
+neto = Aluno('neto', 'conceição')
+neto.info()
--- Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/Herança/pessoa.py
+++ Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/Herança/pessoa.py
@@ -3,7 +3,7 @@ class Pessoa:
         self.nome = nome
         self.sobrenome = sobrenome
 
-    def falar_nome_class(self):
+    def info(self):
         print(f'{self.nome}, {self.sobrenome}, {self.__class__.__name__}')
 
 class Cliente(Pessoa):
@@ -12,11 +12,3 @@ class Cliente(Pessoa):
 
 class Aluno(Cliente):
     ...
-
-carlos = Cliente('carlos', 'conceição')
-print(f'{carlos.nome.title()} {carlos.sobrenome.title()}')
-carlos.falar_nome_class()
-
-mayara = Aluno('mayara', 'ramos')
-print(f'{mayara.nome.title()} {mayara.sobrenome.title()}')
-mayara.falar_nome_class()
