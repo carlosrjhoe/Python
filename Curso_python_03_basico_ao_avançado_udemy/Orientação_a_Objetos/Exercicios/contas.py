ed1e8096cec1162925f8c7fd4508852e73adc6a8
Carlos Roberto <carlosrjhoe@gmail.com>
2024-02-18 22:02:07 -0300

    Criando classe conta corrente

--- Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/Exercicios/contas.py
+++ Curso_python_03_basico_ao_avançado_udemy/Orientação_a_Objetos/Exercicios/contas.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 class Conta(ABC):
-    def __init__(self, agencia, conta, saldo):
+    def __init__(self, agencia, conta, saldo=0):
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
@@ -15,15 +15,32 @@ class Conta(ABC):
         self.detalhes(f'DEPOSITANDO R${valor:.2f}')
 
     def detalhes(self, msg=''):
-        print(f'O seu saldo: R${self.saldo:.2f} - {msg}')
+        print(f'O seu saldo: R${self.saldo:.2f} {msg}')
 
 class ContaPoupanca(Conta):
-    
     def sacar(self, valor):
         valor_pos_saque = self.saldo - valor
         if valor_pos_saque >= 0:
             self.saldo -= valor
             self.detalhes(f'SACANDO R${valor:.2f}')
             return self.saldo
+        print(f'Não foi possivel sacar valor desejado.')
+        self.detalhes(f'SAQUE DE R${valor:.2f} NEGADO')
+
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo=0, limite=0):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+        
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+        limite_maximo =- self.limite
+        
+        if valor_pos_saque >= limite_maximo:
+            self.saldo -= valor
+            self.detalhes(f'SACANDO R${valor:.2f}')
+            return self.saldo
+        
+        print(f'Seu limite é {self.limite}')
         print(f'Não foi possivel sacar')
         self.detalhes(f'SAQUE NEGADO')
