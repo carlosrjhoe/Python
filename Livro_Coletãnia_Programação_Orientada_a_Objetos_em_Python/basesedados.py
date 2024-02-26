bac698676b4d888eacdb942f020b4c2cf55a1a84
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-02-26 05:36:25 -0300

    Livro Colentãnia orienta a objetos em python: Encapsulamento (Página-75)

---
+++ Livro_Coletãnia_Programação_Orientada_a_Objetos_em_Python/basesedados.py
@@ -0,0 +1,4 @@
+class BaseDeDados:
+   def __init__(self):
+        self.__dados = {}
+        
--- Livro_Coletãnia_Programação_Orientada_a_Objetos_em_Python/main.py
+++ Livro_Coletãnia_Programação_Orientada_a_Objetos_em_Python/main.py
@@ -1,10 +1,4 @@
-from classe import Pessoa
+from basesedados import BaseDeDados
 
-pessoa01 = Pessoa('carlos', 38, 370, 'M', 18.1)
-pessoa02 = Pessoa('mayara', 39, 'R$280', 'F', 17.0)
-
-pessoa01.desconto(15)
-pessoa02.desconto(20)
-
-print(pessoa01.preco)
-print(pessoa02.preco)
+dados = BaseDeDados()
+print(dados.)
