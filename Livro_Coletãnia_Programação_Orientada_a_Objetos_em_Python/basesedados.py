c5e591d10bc18ad9a98b7783a2bab27176c91659
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-02-27 18:40:20 -0300

    Livro Colentãnia orienta a objetos em python: Encapsulamento (página 86)

--- Livro_Coletãnia_Programação_Orientada_a_Objetos_em_Python/basesedados.py
+++ Livro_Coletãnia_Programação_Orientada_a_Objetos_em_Python/basesedados.py
@@ -1,4 +1,16 @@
 class BaseDeDados:
-   def __init__(self):
-        self.__dados = {}
-        
+    def __init__(self):
+        self.__base = {}
+
+    def inserir(self, nome, fone):
+        if 'clientes' not in self.__base:
+            self.__base['clientes'] = {nome:fone}
+        else:
+            self.__base['clientes'].update({nome:fone})
+
+    def listar(self):
+        for nome, fone in self.__base['clientes'].items():
+            print(f'Nome: {nome.title()} - Fone: {fone}')
+
+    def apagar(self, nome):
+        del self.__base['clientes'][nome]
--- Livro_Coletãnia_Programação_Orientada_a_Objetos_em_Python/main.py
+++ Livro_Coletãnia_Programação_Orientada_a_Objetos_em_Python/main.py
@@ -1,4 +1,12 @@
 from basesedados import BaseDeDados
 
-dados = BaseDeDados()
-print(dados.)
+relClientes = BaseDeDados()
+relClientes.inserir('carlos', 123456789)
+relClientes.inserir('mayara', 12345687689)
+relClientes.inserir('neto', 1234539369)
+relClientes.inserir('luna', 1234539276)
+relClientes.listar()
+
+relClientes.__base = 'Novo Banco de dados'
+print(relClientes._baseDeDados__base)
+print(relClientes.__base)
