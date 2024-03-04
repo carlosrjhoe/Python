fadc9a2b311e2de39cc9f067e5bf552316c2c492
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-04 08:31:01 -0300

    Livro Colentãnia orienta a objetos em python: Acoplamento e desacoplamento de classes e criando mais classes

--- Livro_Coletãnia_Programação_Orientada_a_Objetos_em_Python/Tópicos Avançados/frota.py
+++ Livro_Coletãnia_Programação_Orientada_a_Objetos_em_Python/Tópicos Avançados/frota.py
@@ -13,10 +13,14 @@ class Num002(VeiculoBase):
     def atender_solicitacao(self) -> None:
         print('Veículo n° 002 em deslocamento até o cliente')
 
-class Veiculo:
-    def __init__(self, id):
-        self.num = self.chama_veiculo(id)
-        
+class Veiculo(ABC):
+    @staticmethod
+    @abstractmethod
+    def chama_veiculo(num: str) -> VeiculoBase:
+        pass
+
+class Veiculo_2(Veiculo):
+    @staticmethod
     def chama_veiculo(num: str) -> VeiculoBase:
         if num == '001':
             return Num001()
@@ -24,11 +28,19 @@ class Veiculo:
             return Num002()
         assert 0, 'Veiculo indisponível'
 
-    def atender_solicitacao(self):
-        self.num.atender_solicitacao()
+class Veiculo_3(Veiculo):
+    @staticmethod
+    def chama_veiculo(num: str) -> VeiculoBase:
+        if num == '003':
+            return Num001()
+        if num == '004':
+            return Num002()
+        if num == '005':
+            return Num002()
+        assert 0, 'Veiculo indisponível'
 
 if __name__ == "__main__":
-    frota_disponivel = ['001', '002']
+    frota_disponivel = ['001', '002', '003', '004', '005']
     print(f'Veículos disponíveis: {frota_disponivel}')
     cliente_id_237 = str(input('Escolha um veículo: '))
     if cliente_id_237 == '001':
