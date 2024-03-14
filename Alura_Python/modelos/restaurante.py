c1014febc44c477efdf27c1937e7d9e10c1303d8
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-14 19:24:05 -0300

    Curso Python OO: Property

--- Alura_Python/modelos/restaurante.py
+++ Alura_Python/modelos/restaurante.py
@@ -5,15 +5,20 @@ class Restaurante:
     def __init__(self, nome, categoria, ativo=False):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = ativo
+        self._ativo = ativo
         Restaurante.restaurantes.append(self)
+
+    @property
+    def ativo(self):
+        return '✔️' if self._ativo else '❎'
 
     def __str__(self) -> str:
         return f'{self.nome} - {self.categoria} - {self.ativo}'
 
     def listar_restaurantes() -> list:
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
 
             
 if __name__ == "__main__":
