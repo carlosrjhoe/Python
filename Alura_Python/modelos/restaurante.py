548e4fe0296b12db120c076ef683a35652b02ce1
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-14 19:45:12 -0300

    Curso Python OO: Método de classe

--- Alura_Python/modelos/restaurante.py
+++ Alura_Python/modelos/restaurante.py
@@ -12,17 +12,23 @@ class Restaurante:
     def ativo(self):
         return '✔️' if self._ativo else '❎'
 
+    def alterar_estado(self):
+        self._ativo = not self._ativo
+
     def __str__(self) -> str:
         return f'{self._nome} - {self.categoria} - {self.ativo}'
 
-    def listar_restaurantes() -> list:
+    @classmethod
+    def listar_restaurantes(cls) -> list:
         print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante._ativo}')
+
 
             
 if __name__ == "__main__":
     restaurante_1 = Restaurante('restaurante python', 'fastFood')
+    restaurante_1.alterar_estado()
     restaurante_2 = Restaurante('restaurante java', 'pizza express')
 
     Restaurante.listar_restaurantes()
