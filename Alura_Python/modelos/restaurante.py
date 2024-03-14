7744118b7dda00d9e871563ede7481d5cd449405
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-14 19:09:18 -0300

    Curso Python OO: Criando nossos metodos

--- Alura_Python/modelos/restaurante.py
+++ Alura_Python/modelos/restaurante.py
@@ -1,13 +1,23 @@
 class Restaurante:
+
+    restaurantes = []
     
     def __init__(self, nome, categoria, ativo=False):
         self.nome = nome
         self.categoria = categoria
         self.ativo = ativo
+        Restaurante.restaurantes.append(self)
 
     def __str__(self) -> str:
         return f'{self.nome} - {self.categoria} - {self.ativo}'
 
+    def listar_restaurantes() -> list:
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+
+            
 if __name__ == "__main__":
-    restaurante = Restaurante('Restaurante Python', 'FastFood')
-    print(restaurante)
+    restaurante_1 = Restaurante('Restaurante Python', 'FastFood')
+    restaurante_2 = Restaurante('Restaurante Java', 'Pizza Express')
+
+    Restaurante.listar_restaurantes()
