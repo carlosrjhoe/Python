cece14143049f4d6adb5c2cfd7f9fbde4d44fbef
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-16 09:48:46 -0300

    Curso Adicionando regra de negócio na avaliação

---
+++ Alura_Python/exercicio.py
@@ -0,0 +1,42 @@
+import os
+
+restaurantes = []
+
+def exibindo_programa():
+    print("""
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+    """)
+
+def finalizar_app():
+    os.system('cls')
+    print('4. Saindo do programa...')
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+    
+    opcao = int(input('Escolha uma opção: '))
+    
+    if opcao == 1:
+        print('1. Cadastrar restaurante')
+    elif opcao == 2:
+        print('2. Listar restaurante')
+    elif opcao == 3:
+        print('3. Ativar restaurante')
+    else:
+        finalizar_app()
+
+        
+def main():
+    exibindo_programa()
+    exibir_opcoes()
+
+if __name__ == "__main__":
+    main()
