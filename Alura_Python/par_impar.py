09fc616a20d8841d4638c4d7b258a85aadad83c7
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-16 18:45:45 -0300

    Resolvendo exercicio 01

---
+++ Alura_Python/par_impar.py
@@ -0,0 +1,13 @@
+def par_impar():
+    num = int(input('Digite um número: '))
+
+    if num % 2 == 0:
+        print('PAR')
+    else:
+        print('IMPAR')
+
+def main():
+    par_impar()
+
+if __name__ == "__main__":
+    main()
