bbd83ac4e34cd85f2a75b26a941a8c44219f4dda
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-16 19:32:57 -0300

    Adicionando try e except na solução do código

--- Alura_Python/exercicio.py
+++ Alura_Python/exercicio.py
@@ -11,32 +11,41 @@ def exibindo_programa():
     ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
     ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
     """)
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
 def finalizar_app():
     os.system('cls')
     print('4. Saindo do programa...')
 
-def exibir_opcoes():
-    print('1. Cadastrar restaurante')
-    print('2. Listar restaurante')
-    print('3. Ativar restaurante')
-    print('4. Sair\n')
-    
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    if opcao_escolhida == 1:
-        print('Adicionar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    elif opcao_escolhida == 4:
-        print('Finalizar app')
-        finalizar_app()
-    else:
-        print('Opção inválida!')
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
+def exibir_opcoes():
+
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        if opcao_escolhida == 1:
+            print('Adicionar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            print('Finalizar app')
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+        
         
 def main():
+    os.system('cls')
     exibindo_programa()
     exibir_opcoes()
 
