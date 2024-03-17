a6510d25617dd1a5a918b763c05e6b4d6a973db3
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-16 21:58:43 -0300

    Resolvendo exercicios listas, laços e excessões

--- Alura_Python/lista.py
+++ Alura_Python/lista.py
@@ -1,6 +1,9 @@
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numeros = []
 nomes = ['carlos', 'mayara', 'neto', 'luna']
 dados = [1985, 2024]
+soma = 0
+soma_valores = 0
 
 """2 - Crie uma lista e utilize um loop for para percorrer todos os elementos da lista."""
 for numero in numeros:
@@ -26,6 +29,21 @@ for i in range(1, len(numeros)+1):
     print(f'{num} * {i} = {num*i}')
 
 """6 - Crie uma lista de números e utilize um loop for para calcular a soma de todos os elementos. Utilize um bloco try-except para lidar com possíveis exceções."""
-
+try:
+    for numero in numeros:
+        soma += numero
+    print(f'Soma dos elementos: {soma}')
+except Exception as ERRO:
+    print(f'Ocorreu um erro: {ERRO}')
 
 """7 - Construa um código que calcule a média dos valores em uma lista. Utilize um bloco try-except para lidar com a divisão por zero, caso a lista esteja vazia."""
+try:
+    for valor in numeros:
+        soma_valores += valor
+    media = soma_valores/len(numeros)
+    print(f'Média de valores: {media}')
+except ZeroDivisionError:
+    print('A lista está vazia.')
+except Exception as ERRO:
+    print(f'Ocorreu um erro: {ERRO}')
+        
