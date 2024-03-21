d1c1666c39ecf030718d9d92c4682252df316f6e
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-21 20:07:13 -0300

    Curso Python: Verificando requests

---
+++ Alura_Python/app_2.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+response = requests.get(url)
+
+if response.status_code == 200:
+    dados_json = response.json()
+    print(dados_json)
+else:
+    print(f'HTTP error: {response.status_code}')
