5fbf91544cdc91b5e8ded23d41969e026f05bf7e
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-28 20:44:48 -0300

    Imerção Alura Python

--- Alura_Python/Projeto_imerção_Python/aula_02.py
+++ Alura_Python/Projeto_imerção_Python/aula_02.py
@@ -1,5 +1,29 @@
 import pandas as pd
+pd.options.display.float_format = '{:.2f}'.format
+
+df_Principal = pd.read_excel('acoes_pura.xlsx', sheet_name='Principal')
+df_Total_de_acoes = pd.read_excel('acoes_pura.xlsx', sheet_name='Total_de_acoes')
+
+# Ativo	Data	Último (R$)	Var. Dia (%)
+
+df_Principal = df_Principal[['Ativo', 'Data', 'Último (R$)', 'Var. Dia (%)']].copy()
+
+df_Principal = df_Principal.rename(columns={'Último (R$)': 'valor_final', 'Var. Dia (%)': 'var_dia_pct'}).copy()
+
+df_Principal['Var_pct'] = df_Principal['var_dia_pct'] / 100
+
+df_Principal['valor_inicial'] = df_Principal['valor_final'] / (df_Principal['Var_pct'] + 1)
+
+df_Principal = df_Principal.merge(df_Total_de_acoes, left_on='Ativo', right_on='Código', how='left')
+
+df_Principal['variacao_rs'] = (df_Principal['valor_final'] - df_Principal['valor_inicial']) * df_Principal['Qtde. Teórica']
+
+df_Principal['Qtde. Teórica'] = df_Principal['Qtde. Teórica'].astype(int)
+
+df_Principal = df_Principal.rename(columns={'Qtde. Teórica': 'Qtd_teorica'}).copy()
+
+df_Principal = df_Principal.drop(columns=['Código'])
+
 
 if __name__ == "__main__":
-    df_ChatGPT = pd.read_excel('acoes_pura.xlsx', sheet_name='ChatGPT')
-    print(df_ChatGPT)
+    print(df_Principal)
