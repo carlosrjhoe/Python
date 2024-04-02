943f056cb4101e9ef01e73e9f7ed669d7c97539f
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-04-01 21:07:18 -0300

    Livro_Introdução_a_Programação_com_Python: Rastreamento de tempo

--- Livro_Introdução_a_Programação_com_Python/teste.py
+++ Livro_Introdução_a_Programação_com_Python/teste.py
@@ -1,4 +1,17 @@
 texto = "ABCDEFGHI"
-for i in range(len(texto)):
-    print(f'{i} - {texto[i]}')
-    
+# for i in range(len(texto)):
+#     print(f'{i} - {texto[i]}')
+
+# print(texto[3:])
+# print(texto[:3])
+# print(texto[])
+
+divida = 0
+compra = 100
+divida = divida + compra
+compra = 200
+divida = divida + compra
+compra = 300
+divida = divida + compra
+compra = 0
+print(divida)
