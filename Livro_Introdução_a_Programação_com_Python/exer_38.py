2c7961062f7b93513b47484415726f0300e850d5
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-04-03 05:05:31 -0300

    Livro_Introdução_a_Programação_com_Python: resolvendo exercicios

---
+++ Livro_Introdução_a_Programação_com_Python/exer_38.py
@@ -0,0 +1,5 @@
+"""Exercício 3.8 Escreva um programa que leia um valor em metros e o exiba convertido em milímetros"""
+
+centimeto = float(input('Digite um número: '))
+milimetro = centimeto * 10
+print(f'Centimetro: {centimeto}cm - Milimetro: {milimetro}mm')
