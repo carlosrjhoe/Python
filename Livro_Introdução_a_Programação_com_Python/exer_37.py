b0d1cf7073edd34676d8afd4207444b2fd90ff5c
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-04-03 04:54:53 -0300

    Livro_Introdução_a_Programação_com_Python: resolvendo exercicios

---
+++ Livro_Introdução_a_Programação_com_Python/exer_37.py
@@ -0,0 +1,7 @@
+"""Exercício 3.7 Faça um programa que peça dois números inteiros. Imprima a soma
+desses dois números na tela"""
+
+num_1 = int(input('Digite um número: '))
+num_2 = int(input('Digite outro número: '))
+soma = num_1 + num_2
+print(f'Soma de {num_1} e {num_2} é = {soma}')
