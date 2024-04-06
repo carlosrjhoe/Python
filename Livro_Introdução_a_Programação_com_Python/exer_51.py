2cc862830d8fa0cbac68cfdc247a32a8ebf72dfd
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-04-06 12:00:17 -0300

    Livro_Introdução_a_Programação_com_Python: resolvendo exercicios

---
+++ Livro_Introdução_a_Programação_com_Python/exer_51.py
@@ -0,0 +1,22 @@
+"""Exercício 5.1 Modifique o programa para exibir os números de 1 a 100"""
+# num = 1
+# while num <= 100:
+#     print(num)
+#     num += 1
+
+"""Exercício 5.2 Modifique o programa para exibir os números de 50 a 100"""
+
+# num = 1
+# while num <= 100:
+#     if num >= 50:
+#         print(num)
+#     num += 1
+
+"""Exercício 5.3 Faça um programa para escrever a contagem regressiva do lançamento
+de um foguete. O programa deve imprimir 10, 9, 8, ..., 1, 0 e Fogo! na tela"""
+
+num = 10
+while num >= 0:
+    print(num)
+    num -= 1
+print('Fogo...')
--- Livro_Introdução_a_Programação_com_Python/exer_test.py
+++ Livro_Introdução_a_Programação_com_Python/exer_test.py
@@ -1,10 +1,15 @@
-salario = float(input('Digite o salário para cálculo do imposto de renda: '))
-base = salario
-imposto = 0
+# salario = float(input('Digite o salário para cálculo do imposto de renda: '))
+# base = salario
+# imposto = 0
 
-if base > 3000:
-    imposto = imposto + ((base - 3000) * 0.35)
-    base = 3000
-if base > 1000:
-    imposto = imposto + ((base - 1000) * 0.20)
-print(f'Sálario R${salario:.2f} Imposto a pagar R$ {imposto:.2f}')
+# if base > 3000:
+#     imposto = imposto + ((base - 3000) * 0.35)
+#     base = 3000
+# if base > 1000:
+#     imposto = imposto + ((base - 1000) * 0.20)
+# print(f'Sálario R${salario:.2f} Imposto a pagar R$ {imposto:.2f}')
+
+x = 0
+while x <= 3:
+    print(x)
+    x += 1
