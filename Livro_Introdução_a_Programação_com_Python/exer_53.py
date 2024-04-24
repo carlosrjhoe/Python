031b6edbcbb71b0d22eac177349bcbe0d88ac804
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-04-23 22:11:02 -0300

    Atualizando arquivos

--- Livro_Introdução_a_Programação_com_Python/exer_53.py
+++ Livro_Introdução_a_Programação_com_Python/exer_53.py
@@ -26,15 +26,15 @@ multiplicação do primeiro pelo segundo. Utilize apenas os operadores de soma e
 subtração para calcular o resultado. Lembre-se de que podemos entender a multiplicação de dois 
 números como somas sucessivas de um deles. Assim, 4 × 5 = 5 + 5 + 5 + 5 = 4 + 4 + 4 + 4 + 4"""
 
-# num1 = int(input("Digite o primeiro número: "))
-# num2 = int(input("Digite o segundo número: "))
-# x = 1
-# r = 0
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+x = 1
+r = 0
 
-# while x <= num2:
-#     r += num1
-#     x += 1
-# print(f'{num1} x {num2} = {r}')
+while x <= num2:
+    r += num1
+    x += 1
+print(f'{num1} x {num2} = {r}')
 
 """Exercício 5.9 Escreva um programa que leia dois números. Imprima a divisão
 inteira do primeiro pelo segundo, assim como o resto da divisão. Utilize apenas
@@ -50,7 +50,7 @@ def divisao_inteira_e_resto(dividendo, divisor):
     while resto >= divisor:
         resto -= divisor
         quociente += 1
-    
+
     return quociente, resto
 
 def main():
--- Livro_Introdução_a_Programação_com_Python/exer_test.py
+++ Livro_Introdução_a_Programação_com_Python/exer_test.py
@@ -14,10 +14,25 @@
 #     print(x)
 #     x += 1
 
-""" Listagem 5.7 – Impressão de números pares de 0 até um número digitado pelo usuário"""
+# """ Listagem 5.7 – Impressão de números pares de 0 até um número digitado pelo usuário"""
 
-fim = int(input(''))
-x = 0
-while x <= fim:
-    print(x)
-    x += 1
+# fim = int(input(''))
+# x = 0
+# while x <= fim:
+#     print(x)
+#     x += 1
+
+"""Listagem 5.10 – Contagem de questões corretas"""
+
+pontos = 0
+questao = 1
+while questao <= 3:
+    resposta = input(f'Resposta da questão {questao}: ').upper()
+    if questao == 1 and resposta == 'B':
+        pontos += 1
+    if questao == 2 and resposta == 'A':
+        pontos += 1
+    if questao == 3 and resposta == 'C':
+        pontos += 1
+    questao += 1
+print(f'O aluno fez {pontos} pontos.')
