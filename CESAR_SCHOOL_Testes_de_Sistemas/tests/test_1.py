273729498b93ee416ef644bfb071d28fa5d4f785
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-04-27 14:40:45 -0300

    Criando 3° teste, abrir brouser e clicar no botão login e o browser deve mudar para pagina de produtos

--- CESAR_SCHOOL_Testes_de_Sistemas/tests/test_1.py
+++ CESAR_SCHOOL_Testes_de_Sistemas/tests/test_1.py
@@ -1,20 +1,32 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+import pytest
 
-def test_abrir_browser_e_clicar_no_botao_login():
-    """Abrir browser e clicar no botão login, e verifica se permanece na mesmo browser."""
+class Test_login:
     url = "https://www.saucedemo.com/"
-    driver = Chrome()
-    driver.get(url)
-    driver.find_element(By.XPATH, "//*[@id='login-button']").click()
-    assert driver.current_url == url, 'URL incorreta!'
+    url_produtos = 'https://www.saucedemo.com/inventory.html'
 
-def test_clicar_no_botao_login_e_mostrar_msg_de_erro():
-    """Clicar no botão login, e deve aparecer uma msg de erro."""
-    url = "https://www.saucedemo.com/"
-    driver = Chrome()
-    driver.get(url)
-    driver.find_element(By.XPATH, "//*[@id='login-button']").click()
-    error_message = driver.find_element(By.CLASS_NAME, 'error-message-container')
-    assert error_message.text == 'Epic sadface: Username is required', "Messagem de erro incorreta!"
-    driver.quit()
+    @pytest.fixture
+    def setUp(self):
+        self.driver = Chrome()
+        self.driver.get(self.url)
+        yield
+        self.driver.quit()
+        
+    def test_abrir_browser_e_clicar_no_botao_login(self, setUp):
+        """Abrir browser e clicar no botão login, e verifica se permanece na mesmo browser."""
+        self.driver.find_element(By.XPATH, "//*[@id='login-button']").click()
+        assert self.driver.current_url == self.url, 'URL incorreta!'
+
+    def test_clicar_no_botao_login_e_mostrar_msg_de_erro(self, setUp):
+        """Clicar no botão login, e deve aparecer uma msg de erro."""
+        self.driver.find_element(By.XPATH, "//*[@id='login-button']").click()
+        error_message = self.driver.find_element(By.CLASS_NAME, 'error-message-container')
+        assert error_message.text == 'Epic sadface: Username is required', "Messagem de erro incorreta!"
+
+    def test_efetuar_login_valido_e_deve_exibir_pagina_de_produtos(self, setUp):
+        """Efetuar login valido e deve exibir pagina de produtos"""
+        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")
+        self.driver.find_element(By.XPATH, "//*[@id='login-button']").click()
+        assert self.driver.current_url == self.url_produtos, 'URL incorreta!'
