fabdc82064d35967346c68a7bc7ef5690e1f9268
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-04-27 11:11:41 -0300

    Criando 2° teste, clicar no botão login, e deve mostrar uma mensagem de erro

--- CESAR_SCHOOL_Testes_de_Sistemas/tests/test_1.py
+++ CESAR_SCHOOL_Testes_de_Sistemas/tests/test_1.py
@@ -2,6 +2,19 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
 def test_abrir_browser_e_clicar_no_botao_login():
+    """Abrir browser e clicar no botão login, e verifica se permanece na mesmo browser."""
+    url = "https://www.saucedemo.com/"
     driver = Chrome()
-    driver.get('https://www.saucedemo.com/')
-    driver.find_element(By.ID, "login-button").click()
+    driver.get(url)
+    driver.find_element(By.XPATH, "//*[@id='login-button']").click()
+    assert driver.current_url == url, 'URL incorreta!'
+
+def test_clicar_no_botao_login_e_mostrar_msg_de_erro():
+    """Clicar no botão login, e deve aparecer uma msg de erro."""
+    url = "https://www.saucedemo.com/"
+    driver = Chrome()
+    driver.get(url)
+    driver.find_element(By.XPATH, "//*[@id='login-button']").click()
+    error_message = driver.find_element(By.CLASS_NAME, 'error-message-container')
+    assert error_message.text == 'Epic sadface: Username is required', "Messagem de erro incorreta!"
+    driver.quit()
