9ab6bb9b6a7dfe8f298ca3e454f5684d25cf731b
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-04-28 14:03:06 -0300

    BEECROWD: Exercicio 1019

---
+++ Beecrowd/Iniciante/BEE1021.py
@@ -0,0 +1,17 @@
+def main():
+    reais, centavos = map(int, input('').split('.'))
+    centavos = centavos + reais*100
+    notas = [100, 50, 20, 10, 5, 2]
+    moedas = [100, 50, 25, 10, 5, 1]
+    
+    print('NOTAS:')
+    for nota in notas:
+        print(f'{centavos//(nota*100)} nota(s) de R$ {nota}.00')
+        centavos = centavos%(nota*100)
+
+    print('MOEDAS:')
+    for moeda in moedas:
+        print(f'{centavos//moeda} moeda(s) de R$ {moeda//100}.{moeda%100:02}')
+        centavos = centavos % moeda
+if __name__ == '__main__':
+    main()
