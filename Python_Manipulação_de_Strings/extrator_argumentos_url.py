709cc5cb743526e391df472d4aacfd01e2cd3969
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-05-09 04:55:38 -0300

    Python: Manipulação de strings - fatiando string

--- Python_Manipulação_de_Strings/extrator_argumentos_url.py
+++ Python_Manipulação_de_Strings/extrator_argumentos_url.py
@@ -11,3 +11,13 @@ class ExtratorArgumentoUrl:
             return True
         else:
             return False
+
+    def extrair_argumento(self):
+        indice_inicial_moeda_destino = self.url.find('=',15)+1
+        indice_inicial_moeda_origem = self.url.find('=')+1
+        indice_final_moeda_origem = self.url.find('&')
+
+        moeda_origem = self.url[indice_inicial_moeda_origem:indice_final_moeda_origem]
+        moeda_destino = self.url[indice_inicial_moeda_destino:]
+        return moeda_origem, moeda_destino
+    
--- Python_Manipulação_de_Strings/main.py
+++ Python_Manipulação_de_Strings/main.py
@@ -1,7 +1,11 @@
 from extrator_argumentos_url import ExtratorArgumentoUrl
 
-# url = 'https://www.xvideos.com/video.udtumdoe426/charlotte_garconete_safada_de_40_anos'
-url = None
+url = 'moedaorigem=real&moedadestino=dolar'
+# url = None
 
 # ExtratorArgumentoUrl.url_eh_valida((url))
-print(ExtratorArgumentoUrl.url_eh_valida(url))
+# print(ExtratorArgumentoUrl.url_eh_valida(url))
+argumento = ExtratorArgumentoUrl(url)
+moeda_origem, moeda_destino = argumento.extrair_argumento()
+print(moeda_origem)
+print(moeda_destino)
