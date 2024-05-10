519eb2ff9bd8fffd024e7c85d52d5c1837225539
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-05-10 20:27:47 -0300

    Estudando pytest, usando visibility_of_element_located

--- CESAR_SCHOOL_Testes_de_Sistemas/tests/test_2.py
+++ CESAR_SCHOOL_Testes_de_Sistemas/tests/test_2.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from conftest import url
 
 class Test_logout:
@@ -6,5 +8,7 @@ class Test_logout:
     def test_logout(self, login_app):
         driver = login_app
         driver.find_element(By.ID, "react-burger-menu-btn").click()
+        menu_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(By.ID, "react-burger-menu-btn"))
+        assert menu_element.is_displayed(), 'Menu não foi aberto!'
+        
         driver.find_element(By.ID, "logout_sidebar_link").click()
-        assert driver.current_url == url, 'URL incorreta!'
