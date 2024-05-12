da78f8190c5563230c4e55d94586090ca011e323
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-05-12 10:19:10 -0300

    Estudando Fextures

--- Curso_Fixtures_Eduardo_Mendes/app/__init__.py
+++ Curso_Fixtures_Eduardo_Mendes/app/__init__.py
@@ -1,3 +1,19 @@
+class Tarefa:
+    def __init__(self, nome):
+        self.nome = nome
+
 class Quadro:
-    def __init__(self, colunas=[]):
+    def __init__(self, colunas=[], tarefas=[]):
         self.colunas = colunas
+        self.tarefas = tarefas
+
+    def inserir_coluna(self, coluna):
+        self.colunas.append(coluna)
+
+    def inserir_tarefa(self, tarefa):
+        self.tarefas.append(tarefa)
+
+
+class Coluna:
+    def __init__(self, nome):
+        self.nome = nome
--- Curso_Fixtures_Eduardo_Mendes/tests/test_do_quadro.py
+++ Curso_Fixtures_Eduardo_Mendes/tests/test_do_quadro.py
@@ -1,6 +1,28 @@
-from app import Quadro
+from app import Quadro, Coluna, Tarefa
+from pytest import fixture
 
-def test_não_deve_existir_coluna_no_quadro():
-    quadro = Quadro() # Dado
+@fixture
+def setUp():
+    print('Estou montando o contexto!')
+    return Quadro()
+
+
+def test_não_deve_existir_coluna_no_quadro(setUp):
+    quadro = setUp # Dado
     quantidade_colunas = len(quadro.colunas)
     assert quantidade_colunas == 0
+
+def test_quando_inserir_uma_tarefa_no_quadro_deve_estar_na_primeira_coluna(setUp):
+    quadro = setUp # Dado
+    quadro.inserir_tarefa(Tarefa(nome='Dormir'))
+
+def test_quando_inserir_uma_coluna_deve_existir_uma_coluna(setUp):
+    quadro = setUp # Dado
+    quadro.inserir_coluna(Coluna(nome='A fazer')) #
+    quantidade_de_colunas = quadro.colunas #
+    assert len(quantidade_de_colunas) == 1
+
+def test_quando_inserir_uma_coluna_A_Fazer_ela_deve_estar_no_quadro(setUp):
+    quadro = setUp # Dado
+    quadro.inserir_coluna(Coluna(nome='A fazer')) #
+    assert quadro.colunas[0].nome == 'A fazer'
