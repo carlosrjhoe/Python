9f9ace34ff368fc2d3694f9c3dd9867c9a468312
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-05-26 22:07:38 -0300

    Livro Pense em Python: Capítulo 01

---
+++ Livro_pense_em_python/capitulo_1/primeiro_programa.py
@@ -0,0 +1,42 @@
+# Instrução de impressão
+# texto = 'Hello, World!'
+# print(f'{texto}')
+# print(f'{type(texto)}')
+
+# Operadores aritméticos
+num_1, num_2, = 3, 2
+
+# Operador de Soma
+print(f'Soma -> {num_1} + {num_2} = {num_1 + num_2}')
+print(f'{type(num_1 + num_2)}')
+print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+
+# # Operador de Subtração
+# print(f'Subtração -> {num_1} - {num_2} = {num_1 - num_2}')
+# print(f'{type(num_1 - num_2)}')
+# print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+
+# # Operador de Multiplicação
+# print(f'Multiplicação -> {num_1} x {num_2} = {num_1 * num_2}')
+# print(f'{type(num_1 * num_2)}')
+# print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+
+# # Operador de Divisão
+# print(f'Divisão -> {num_1} / {num_2} = {num_1 / num_2}')
+# print(f'{type(num_1 / num_2)}')
+# print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+
+# # Resto da Divisão
+# print(f'Resto da Divisão -> {num_1} % {num_2} = {num_1 % num_2}')
+# print(f'{type(num_1 % num_2)}')
+# print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+
+# # Inteiro da Divisão
+# print(f'Inteiro da Divisão -> {num_1} // {num_2} = {num_1 // num_2}')
+# print(f'{type(num_1 // num_2)}')
+# print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+
+# # Exponenciação
+# print(f'Exponenciação -> {num_1} ** {num_2} = {num_1 ** num_2}')
+# print(f'{type(num_1 ** num_2)}')
+# print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
