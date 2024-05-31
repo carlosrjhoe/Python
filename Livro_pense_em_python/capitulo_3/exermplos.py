20eefa448a50b25cb08ecdf814f9e225287b04f9
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-05-31 20:45:38 -0300

    Livro Pense em Python: Capítulo 03

---
+++ Livro_pense_em_python/capitulo_3/exermplos.py
@@ -0,0 +1,21 @@
+# def exibir_texto():
+#     print('1° Função Cabo de Santo Agostinho')
+    
+# def repitir_texto():
+#     print('2° Função Recife cidade do brega')
+#     exibir_texto()
+
+
+# def print_twice(n):
+#     print(n)
+#     print(n)
+
+def cat_twice(cat_1, cat_2):
+    cats = cat_1 + cat_2
+    print(cats)
+    
+if __name__ == '__main__':
+    cat1 = 'alfred'
+    cat2 = 'jose'
+    cat_twice(cat1, cat2)
+    print(cats)
