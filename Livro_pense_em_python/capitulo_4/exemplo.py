08e188749e9e52f2fdcf23d387850386001fd111
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-06-01 09:42:43 -0300

    Livro Pense em Python: Capítulos 04 e 05

--- Livro_pense_em_python/capitulo_4/exemplo.py
+++ Livro_pense_em_python/capitulo_4/exemplo.py
@@ -1,10 +1,21 @@
 from turtle import Turtle
 
-def desenho_quadrado():
-    caneta = Turtle()
+def draw_square(length, angulate):
+    pencil = Turtle()
     for i in range(4):
-        caneta.fd(100)
-        caneta.lt(90)
+        pencil.fd(length)
+        pencil.lt(angulate)
+
+def draw_polygon(length, side, angulate):
+    pencil = Turtle()
+    pencil.speed(1)
+    for _ in range(side):
+        pencil.fd(length)
+        pencil.forward(length)
+        pencil.lt(angulate)
 
 if __name__ == '__main__':
-    desenho_quadrado()
+    length = 10
+    side = 5
+    angulate = side / 36
+    draw_polygon(length, side, angulate)
