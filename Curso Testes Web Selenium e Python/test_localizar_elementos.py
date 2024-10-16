ed97dd523b9ea0bc5bcb520bba796db8aa2910bd
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-10-15 21:46:54 -0300

    Estudando python e comando de iteração com elementos

--- Curso Testes Web Selenium e Python/test_localizar_elementos.py
+++ Curso Testes Web Selenium e Python/test_localizar_elementos.py
@@ -1,15 +1,26 @@
-from time import sleep
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
 driver = Chrome()
-url_saucedemo = 'https://www.saucedemo.com/'
-driver.get(url_saucedemo)
+URL_SAUCEDEMO = 'https://www.saucedemo.com/'
+driver.get(URL_SAUCEDEMO)
 
 user_name = driver.find_element(By.ID, 'user-name')
 password = driver.find_element(By.ID, 'password')
 botao = driver.find_element(By.ID, 'login-button')
 
+# print(user_name.is_displayed())
+
 user_name.send_keys('standard_user')
 password.send_keys('secret_sauce')
 botao.click()
+
+# elementos = driver.find_elements(By.XPATH, '//*[@class="inventory_item_name "]')
+
+elem_products = driver.find_element(By.XPATH, "//span[@class='title']")
+assert elem_products.text == 'Products'
+
+img_backpack = driver.find_element(
+    By.XPATH, "(//img[@class='inventory_item_img'])[1]")
+print(img_backpack.get_attribute('alt'))
+assert img_backpack.get_attribute('alt') == 'Sauce Labs Backpack'
