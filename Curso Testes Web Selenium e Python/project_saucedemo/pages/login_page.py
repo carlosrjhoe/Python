cdd2dd64475eb2cde1f9a68fee45e1d8b6316cd1
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-11-04 20:16:34 -0300

    REFACTOR: pep8

--- Curso Testes Web Selenium e Python/project_saucedemo/pages/login_page.py
+++ Curso Testes Web Selenium e Python/project_saucedemo/pages/login_page.py
@@ -12,18 +12,22 @@ class LoginPage:
         self.user_name_input = (By.XPATH, '//input[@id="user-name"]')
         self.password_input = (By.XPATH, '//input[@id="password"]')
         self.login_button = (By.XPATH, '//input[@id="login-button"]')
-        self.erro_message = (By.XPATH, '//div[@class="error-message-container error"]')
+        self.erro_message = (
+            By.XPATH, '//div[@class="error-message-container error"]')
 
     def open(self):
         self.driver.get(self.URL)
 
     def login(self):
-        self.driver.find_element(*self.user_name_input).send_keys(self.username)
+        self.driver.find_element(
+            *self.user_name_input).send_keys(self.username)
         self.driver.find_element(*self.password_input).send_keys(self.password)
         self.driver.find_element(*self.login_button).click()
 
     def invalid_login(self):
-        self.driver.find_element(*self.user_name_input).send_keys(self.username)
-        self.driver.find_element(*self.password_input).send_keys(self.invalid_password)
+        self.driver.find_element(
+            *self.user_name_input).send_keys(self.username)
+        self.driver.find_element(
+            *self.password_input).send_keys(self.invalid_password)
         self.driver.find_element(*self.login_button).click()
         return self.driver.find_element(*self.erro_message)
