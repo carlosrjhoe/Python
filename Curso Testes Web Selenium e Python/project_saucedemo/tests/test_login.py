c1317c5b9e1edca66f372c2144462c8a73791150
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-11-04 20:01:38 -0300

    REFACTOR: adding mark to tests

--- Curso Testes Web Selenium e Python/project_saucedemo/tests/test_login.py
+++ Curso Testes Web Selenium e Python/project_saucedemo/tests/test_login.py
@@ -2,6 +2,7 @@ import pytest
 from pages.login_page import LoginPage
 from pages.products_page import ProductsPage
 
+@pytest.mark.login
 def test_login_sucess(setUp):
     login_page = LoginPage(setUp)
     Object_product_page = ProductsPage(setUp)
@@ -9,6 +10,7 @@ def test_login_sucess(setUp):
     login_page.login()
     assert Object_product_page.is_products_title_dispayed() == "Products"
 
+@pytest.mark.login
 def test_invalid_login(setUp):
     login_page = LoginPage(setUp)
     login_page.open()
