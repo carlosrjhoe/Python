d0746edaeb3871cdf0e189981822e29b38799a29
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-11-06 21:47:23 -0300

    TEST: test_verify_the_product_name_in_the_cart PASSED

--- Curso Testes Web Selenium e Python/project_saucedemo/pages/products_page.py
+++ Curso Testes Web Selenium e Python/project_saucedemo/pages/products_page.py
@@ -9,11 +9,22 @@ class ProductsPage(LoginPage):
     def __init__(self, driver):
         self.driver = driver
         self.products_title = (By.XPATH, '//span[@class="title"]')
-        self.product_list = (By.XPATH, '//button[@class="btn btn_primary btn_small btn_inventory "]')
-        self.product = (By.XPATH, '//button[@id="add-to-cart-sauce-labs-backpack"]')
+        self.product_list = (
+            By.XPATH, '//button[@class="btn btn_primary btn_small btn_inventory "]')
+        self.product = (
+            By.XPATH, '//button[@id="add-to-cart-sauce-labs-backpack"]')
         self.shopping_cart = (By.XPATH, '//span[@class="shopping_cart_badge"]')
+        self.shopping_cart_button = (By.XPATH, '//a[@class="shopping_cart_link"]')
+        self.shopping_cart_iventory_button = (
+            By.XPATH, '//a[@class="shopping_cart_link"]')
+        self.shopping_cart_item = (
+            By.XPATH, '//div[@class="inventory_item_name"]')
+        self.iventory_items = (
+            By.XPATH, '//div[@class="inventory_item_name "]')
+        self.iventory_item_button = (By.XPATH, '//button[@id="add-to-cart"]')
+        self.inventory_item_name = (
+            By.XPATH, '//div[@data-test="inventory-item-name"]')
         super().__init__(driver)
-
 
     def is_products_title_dispayed(self):
         return self.driver.find_element(*self.products_title).text
@@ -23,7 +34,29 @@ class ProductsPage(LoginPage):
         chosen_element = choice(elements)
         chosen_element.click()
 
+    def choose_inventory_item_name(self):
+        items = self.driver.find_elements(*self.iventory_items)
+        chosen_item = choice(items)
+        chosen_item.click()
+
+    def get_inventory_item_name(self):
+        item = self.driver.find_element(*self.inventory_item_name).text
+        return item
+
+    def click_iventory_item_button(self):
+        return self.driver.find_element(*self.iventory_item_button).click()
+
+    def click_shopping_cart_inventory_button(self):
+        return self.driver.find_element(*self.shopping_cart_iventory_button).click()
+
+    def click_shopping_cart(self):
+        return self.driver.find_element(*self.shopping_cart_button).click()
+
+    def get_shopping_cart_item_name(self):
+        return self.driver.find_element(*self.shopping_cart_item).text
+
     def check_shopping_cart(self):
         wait = WebDriverWait(self.driver, 5)
-        quantity = wait.until(EC.visibility_of_element_located(self.shopping_cart))
+        quantity = wait.until(
+            EC.visibility_of_element_located(self.shopping_cart))
         return quantity.text
--- Curso Testes Web Selenium e Python/project_saucedemo/tests/test_products.py
+++ Curso Testes Web Selenium e Python/project_saucedemo/tests/test_products.py
@@ -1,5 +1,6 @@
 import pytest
 from pages.products_page import ProductsPage
+# from time import sleep
 
 def test_add_product_to_shopping_cart(setUp):
     product_page = ProductsPage(setUp)
@@ -7,3 +8,14 @@ def test_add_product_to_shopping_cart(setUp):
     product_page.login()
     product_page.choose_product()
     assert product_page.check_shopping_cart() == '1'
+
+def test_verify_the_product_name_in_the_cart(setUp):
+    product_page = ProductsPage(setUp)
+    product_page.open()
+    product_page.login()
+    product_page.choose_inventory_item_name()
+    product_name = product_page.get_inventory_item_name()
+    product_page.click_iventory_item_button()
+    product_page.click_shopping_cart_inventory_button()
+    product_shopping_cart_name = product_page.get_shopping_cart_item_name()
+    assert product_name == product_shopping_cart_name
