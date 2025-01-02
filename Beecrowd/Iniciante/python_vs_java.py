714da29ba0eeb3a43d757b2f5ff6424e1e208545
Carlos Roberto <carlosrjhoe@gmail.com>
2025-01-02 04:30:32 -0300

    testes

---
+++ Beecrowd/Iniciante/python_vs_java.py
@@ -0,0 +1,7 @@
+widthLand = float(input())
+lengthLand = float(input())
+priceMeterLand = float(input())
+areaSquareMeters = widthLand * lengthLand
+priceSquareMeterLand = areaSquareMeters * priceMeterLand
+print(f'AREA = {areaSquareMeters}')
+print(f'PRICE = R${priceSquareMeterLand}')
