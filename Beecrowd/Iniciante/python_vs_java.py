662ec5f258d94cced409aadf6153ee1f52f41eb5
Carlos Roberto <carlosrjhoe@gmail.com>
2025-01-02 04:48:55 -0300

    python vs java

--- Beecrowd/Iniciante/python_vs_java.py
+++ Beecrowd/Iniciante/python_vs_java.py
@@ -1,7 +1,17 @@
-widthLand = float(input())
-lengthLand = float(input())
-priceMeterLand = float(input())
-areaSquareMeters = widthLand * lengthLand
-priceSquareMeterLand = areaSquareMeters * priceMeterLand
-print(f'AREA = {areaSquareMeters}')
-print(f'PRICE = R${priceSquareMeterLand}')
+class Land:
+    def __init__(self, widthland, lengthland):
+        self.widthland = widthland
+        self.lengthland = lengthland
+        self.price_land = 200.0
+
+    def price_meter_land(self):
+        return self.widthland * self.lengthland
+
+    def area_square_meters(self):
+        return self.price_meter_land() * self.price_land
+
+
+if __name__ == '__main__':
+    land = Land(10.0, 30.0)
+    print(f'AREA = {land.price_meter_land()}')
+    print(f'PRICE = R${land.area_square_meters()}')
