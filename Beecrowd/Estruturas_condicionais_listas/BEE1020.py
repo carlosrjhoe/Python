d1697bc88626b7c245abb0fba101cd5899591a3c
Carlos Roberto <carlosrjhoe@gmail.com>
2025-01-06 05:48:32 -0300

    FEAT: BEE1020 PASSED

--- Beecrowd/Estruturas_condicionais_listas/BEE1020.py
+++ Beecrowd/Estruturas_condicionais_listas/BEE1020.py
@@ -1,7 +1,18 @@
-idade_dias = int(input())
-anos = int(idade_dias / 365)
-meses = int((idade_dias % 365) / 30)
-dias = int((idade_dias % 365) % 30)
-print(f'{anos} ano(s)')
-print(f'{meses} mes(es)')
-print(f'{dias} dia(s)')
+def get_idade_dias_em_meses_anos(valor):
+    anos = valor // 365
+    resto_anos = int(valor % 365)
+    meses = int(resto_anos // 30)
+    dias = int(resto_anos % 30)
+    return anos, meses, dias
+
+
+def main():
+    valor = int(input())
+    ano, meses, dias = get_idade_dias_em_meses_anos(valor)
+    print(f'{ano} ano(s)')
+    print(f'{meses} mes(es)')
+    print(f'{dias} dia(s)')
+
+
+if __name__ == '__main__':
+    main()
