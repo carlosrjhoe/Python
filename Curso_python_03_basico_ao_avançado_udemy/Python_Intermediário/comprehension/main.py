509557e35203fdd269a7b216a396ecc07e9c814c
Carlos Roberto <carlosrjhoe@gmail.com>
2025-01-06 22:59:23 -0300

    FEAT: Estudando Python

--- Curso_python_03_basico_ao_avançado_udemy/Python_Intermediário/comprehension/main.py
+++ Curso_python_03_basico_ao_avançado_udemy/Python_Intermediário/comprehension/main.py
@@ -1,11 +1,32 @@
-# Introdução à List comprehension em Python
+"""Introdução à List comprehension em Python"""
+
 # List comprehension é uma forma rápida para criar listas
 # a partir de iteráveis.
 # print(list(range(10)))
-lista = []
-for numero in range(10):
-    lista.append(numero)
-print(lista)
+# lista = []
+# for numero in range(20):
+#     if numero % 2 == 1:
+#         lista.append(numero)
+# print(lista)
+#
+# lista = [numero * 2 for numero in range(20)]
+# print(lista)
 
-lista = [numero * 2 for numero in range(10)]
-print(lista)
+# Dicionários comprehension em Python
+
+pessoas = {
+    'Nome': 'Carlos Roberto',
+    'Idade': 39,
+    'Profissao': 'Encarregado'
+}
+
+# for chave, valor in Pessoas.items():
+#     print(f'{chave}: {valor}')
+
+dc = {
+    chave: valor.upper()
+    if isinstance(valor, str) else valor
+    for chave, valor in pessoas.items()
+}
+
+print(dc)
